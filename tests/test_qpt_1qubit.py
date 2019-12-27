e537bde02317d6176dcc58fa637b6875ab1ca7e8
komo-fr <komo.mdrms@gmail.com>
2019-12-27 13:46:29 +0900

    refatoring

--- tests/test_qpt_1qubit.py
+++ tests/test_qpt_1qubit.py
@@ -4,52 +4,40 @@ import matlab
 import numpy as np
 
 
-def load_state_list_from_csv(path: str, dim: int = None) -> np.ndarray:
+def load_state_list(path: str, dim: int) -> np.ndarray:
     raw_data = np.loadtxt(path, delimiter=",", dtype=np.complex128)
     num_of_data = int(len(raw_data) / dim)
-    if num_of_data and dim:
-        state_list = np.reshape(raw_data, (num_of_data, dim * dim))
-        return state_list
-    else:
-        return None
+    # TODO: check row of data
+    state_list = np.reshape(raw_data, (num_of_data, dim * dim))
+    return state_list
 
 
-def load_povm_list_from_csv(
-    path: str, dim: int = None, Np: int = None
-) -> (int, np.ndarray):
+def load_povm_list(path: str, dim: int, Np: int) -> (int, np.ndarray):
     raw_data = np.loadtxt(path, delimiter=",", dtype=np.complex128)
     M = int(len(raw_data) / (dim * Np))
-    if Np and M and dim:
-        povm_list = np.reshape(raw_data, (Np, M, dim * dim))
-        return M, povm_list
-    else:
-        return None
+
+    povm_list = np.reshape(raw_data, (Np, M, dim * dim))
+    return M, povm_list
 
 
-def load_schedule_from_csv(path: str) -> (int, np.ndarray):
+def load_schedule(path: str) -> (int, np.ndarray):
     raw_data = np.loadtxt(path, delimiter=",", dtype=np.uint16)
     Nsche, _ = raw_data.shape
     return Nsche, raw_data
 
 
-def load_emp_list_from_csv(path: str, dim: int = None, Nsche: int = None) -> np.ndarray:
+def load_emp_list(path: str, dim: int, Nsche: int) -> np.ndarray:
     raw_data = np.loadtxt(path, delimiter=",", dtype=np.float64)
-    if Nsche and dim:
-        emp_list = np.reshape(raw_data, (Nsche, dim))
-        return emp_list
-    else:
-        return None
+
+    emp_list = np.reshape(raw_data, (Nsche, dim))
+    return emp_list
 
 
-def load_weight_list_from_csv(
-    path: str, Nsche: int = None, M: int = None
-) -> np.ndarray:
+def load_weight_list(path: str, Nsche: int, M: int) -> np.ndarray:
     raw_data = np.loadtxt(path, delimiter=",", dtype=np.float64)
-    if Nsche and M:
-        weight_list = np.reshape(raw_data, (Nsche, M, M))
-        return weight_list
-    else:
-        return None
+
+    weight_list = np.reshape(raw_data, (Nsche, M, M))
+    return weight_list
 
 
 if __name__ == "__main__":
@@ -58,32 +46,28 @@ if __name__ == "__main__":
     csv_path = os.path.dirname(__file__) + "/data/"
 
     print("--- load state list ---")
-    state_np = load_state_list_from_csv(csv_path + "tester_1qubit_state.csv", dim)
+    state_np = load_state_list(csv_path + "tester_1qubit_state.csv", dim)
     state_ml = matlab.double(state_np.tolist(), is_complex=True)
     print(state_ml)
 
     print("--- load povm list ---")
-    M, povm_np = load_povm_list_from_csv(csv_path + "tester_1qubit_povm.csv", dim, Np)
+    M, povm_np = load_povm_list(csv_path + "tester_1qubit_povm.csv", dim, Np)
     povm_ml = matlab.double(povm_np.tolist(), is_complex=True)
     print(f"M={M}")
     print(povm_ml)
 
     print("--- load schedule ---")
-    Nsche, schedule_np = load_schedule_from_csv(csv_path + "schedule_1qubit.csv")
+    Nsche, schedule_np = load_schedule(csv_path + "schedule_1qubit.csv")
     print(f"Nsche={Nsche}")
     schedule_ml = matlab.uint64(schedule_np.tolist())
     print(schedule_ml)
 
     print("--- load emp list ---")
-    emp_list_np = load_emp_list_from_csv(
-        csv_path + "listEmpiDist_2valued.csv", dim, Nsche
-    )
+    emp_list_np = load_emp_list(csv_path + "listEmpiDist_2valued.csv", dim, Nsche)
     emp_list_ml = matlab.double(emp_list_np.tolist())
     print(emp_list_ml)
 
     print("--- load weight list ---")
-    weight_list_np = load_weight_list_from_csv(
-        csv_path + "weight_2valued_uniform.csv", Nsche, M
-    )
+    weight_list_np = load_weight_list(csv_path + "weight_2valued_uniform.csv", Nsche, M)
     weight_list_ml = matlab.double(weight_list_np.tolist())
     print(weight_list_ml)
