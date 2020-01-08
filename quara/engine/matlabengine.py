9b1e10c3f4b536d003c466b2e87aa3605e45c637
komo-fr <komo.mdrms@gmail.com>
2020-01-08 14:30:44 +0900

    [refactoring] change variable name

--- quara/engine/matlabengine.py
+++ quara/engine/matlabengine.py
@@ -10,13 +10,13 @@ class MatlabEngine(object):
         self._matlab_func_path = this_pypath.parent.parent.parent / "matlab"
 
     def __enter__(self):
-        self.eng = matlab.engine.start_matlab()
+        self.engine = matlab.engine.start_matlab()
 
-        self.eng.cd(str(self._matlab_func_path), nargout=0)
+        self.engine.cd(str(self._matlab_func_path), nargout=0)
         for path in self._matlab_func_path.glob("**"):
-            self.eng.addpath(str(path))
+            self.engine.addpath(str(path))
 
-        return self.eng
+        return self.engine
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.eng.quit()
+        self.engine.quit()
