1d4c5c0a9e2ec572b5493443b8b78725def0cc90
snuffkin <tknstyk@gmail.com>
2020-01-08 17:24:39 +0900

    データの整合性チェックを実装

--- quara/protocol/simple_qpt.py
+++ quara/protocol/simple_qpt.py
@@ -10,45 +10,112 @@ logger = logging.getLogger(__name__)
 
 def load_state_list(path: str, dim: int, num_state: int) -> np.ndarray:
     raw_data = np.loadtxt(path, delimiter=",", dtype=np.complex128)
+
+    # check data
+    if raw_data.shape[1] != dim:
+        raise ValueError(
+            f"Invalid number of columns in state list '{path}'. expected={dim}(dim), actual={raw_data.shape[1]}"
+        )
+    if raw_data.shape[0] != num_state * dim:
+        raise ValueError(
+            f"Invalid number of rows in state list '{path}'. expected={dim * num_state}(dim * num_state), actual={raw_data.shape[0]}"
+        )
+
     state_list = np.reshape(raw_data, (num_state, dim * dim))
     return state_list
 
 
 def load_povm_list(path: str, dim: int, num_povm: int, num_outcome: int) -> np.ndarray:
     raw_data = np.loadtxt(path, delimiter=",", dtype=np.complex128)
+
+    # check data
+    if raw_data.shape[1] != dim:
+        raise ValueError(
+            f"Invalid number of columns in povm list '{path}'. expected={dim}(dim), actual={raw_data.shape[1]}"
+        )
+    if raw_data.shape[0] != num_povm * num_outcome * dim:
+        raise ValueError(
+            f"Invalid number of rows in povm list '{path}'. expected={num_povm * num_outcome * dim}(num_povm * num_outcome * dim), actual={raw_data.shape[0]}"
+        )
+
     povm_list = np.reshape(raw_data, (num_povm, num_outcome, dim * dim))
     return povm_list
 
 
-def load_schedule(path: str) -> (int, np.ndarray):
+def load_schedule(path: str, num_state: int, num_povm: int) -> (int, np.ndarray):
     raw_data = np.loadtxt(path, delimiter=",", dtype=np.uint16)
-    num_schedule, _ = raw_data.shape
+    num_schedule = raw_data.shape[0]
+
+    # check data
+    if raw_data.shape[1] != 2:
+        raise ValueError(
+            f"Invalid number of columns in schedule list '{path}'. expected=2, actual={raw_data.shape[1]}"
+        )
+    if np.max(raw_data[:, 0]) > num_state:
+        raise ValueError(
+            f"Invalid state in schedule list '{path}'. expected<={num_state}, actual={np.max(raw_data[:, 0])}"
+        )
+    if np.min(raw_data[:, 0]) >= 0:
+        raise ValueError(
+            f"Invalid state in schedule list '{path}'. expected>=0, actual={np.min(raw_data[:, 0])}"
+        )
+    if np.max(raw_data[:, 1]) > num_povm:
+        raise ValueError(
+            f"Invalid povm in schedule list '{path}'. expected<={num_povm}, actual={np.max(raw_data[:, 1])}"
+        )
+    if np.min(raw_data[:, 0]) >= 0:
+        raise ValueError(
+            f"Invalid povm in schedule list '{path}'. expected>=0, actual={np.min(raw_data[:, 1])}"
+        )
+
     return num_schedule, raw_data
 
 
-def load_empi_list(path: str, num_schedule: int = None) -> (int, np.ndarray):
+def load_empi_list(path: str, num_schedule: int, num_outcome: int) -> np.ndarray:
     raw_data = np.loadtxt(path, delimiter=",", dtype=np.float64)
-    _, num_outcome = raw_data.shape
-    return num_outcome, raw_data
+
+    # check data
+    if raw_data.shape[1] != num_outcome:
+        raise ValueError(
+            f"Invalid number of columns in empi list '{path}'. expected={num_outcome}(num_outcome), actual={raw_data.shape[1]}"
+        )
+    if raw_data.shape[0] != num_schedule:
+        raise ValueError(
+            f"Invalid number of rows in empi list '{path}'. expected={num_schedule}(num_schedule), actual={raw_data.shape[0]}"
+        )
+
+    return raw_data
 
 
-def load_weight_list(path: str, n_schedule: int, num_outcome: int) -> np.ndarray:
+def load_weight_list(path: str, num_schedule: int, num_outcome: int) -> np.ndarray:
     raw_data = np.loadtxt(path, delimiter=",", dtype=np.float64)
-    weight_list = np.reshape(raw_data, (n_schedule, num_outcome, num_outcome))
+
+    # check data
+    if raw_data.shape[1] != num_outcome:
+        raise ValueError(
+            f"Invalid number of columns in weight list '{path}'. expected={num_outcome}(num_outcome), actual={raw_data.shape[1]}"
+        )
+    if raw_data.shape[0] != num_schedule * num_outcome:
+        raise ValueError(
+            f"Invalid number of rows in weight list '{path}'. expected={num_schedule * num_outcome}(num_schedule * num_outcome), actual={raw_data.shape[0]}"
+        )
+
+    weight_list = np.reshape(raw_data, (num_schedule, num_outcome, num_outcome))
     return weight_list
 
 
 def execute(settings: dict) -> np.ndarray:
     logger.debug("--- load schedule ---")
-    num_schedule, schedule_np = load_schedule(settings["path_schedule"])
+    num_schedule, schedule_np = load_schedule(
+        settings["path_schedule"], settings["num_state"], settings["num_povm"]
+    )
     logger.debug(f"num_schedule={num_schedule}")
     schedule_ml = matlab.uint64(schedule_np.tolist())
     logger.debug(schedule_ml)
 
     logger.debug("--- load empi list ---")
-    num_outcome, empi_list_np = load_empi_list(settings["path_empi"], num_schedule)
+    empi_list_np = load_empi_list(settings["path_empi"], num_schedule)
     empi_list_ml = matlab.double(empi_list_np.tolist())
-    logger.debug(f"num_outcome={num_outcome}")
     logger.debug(empi_list_ml)
 
     logger.debug("--- load state list ---")
