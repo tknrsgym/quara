7e5017cb1eba54e2381cd1faf66662e7196554f6
snuffkin <tknstyk@gmail.com>
2020-01-17 13:19:33 +0900

    load_scheduleのテストコードを追加

--- tests/quara/protocol/test_simple_qpt.py
+++ tests/quara/protocol/test_simple_qpt.py
@@ -108,6 +108,85 @@ def test_load_povm_list():
     assert np.array_equal(actual_data, expected_data)
 
 
+def test_load_schedule_invalid_num_column():
+    test_root_dir = Path(os.path.dirname(__file__)).parent.parent
+    path = test_root_dir / "data/schedule_1qubit_invalid_num_column.csv"
+    schedule_np = np.loadtxt(path, delimiter=",", dtype=np.uint16)
+    num_state = np.unique(schedule_np[:, 0]).size
+    num_povm = np.unique(schedule_np[:, 1]).size
+
+    with pytest.raises(ValueError):
+        _, _ = s_qpt.load_schedule(path, num_state, num_povm)
+
+
+def test_load_schedule_invalid_rows():
+    test_root_dir = Path(os.path.dirname(__file__)).parent.parent
+    path = test_root_dir / "data/schedule_1qubit.csv"
+    schedule_np = np.loadtxt(path, delimiter=",", dtype=np.uint16)
+    num_state = np.unique(schedule_np[:, 0]).size
+    num_povm = np.unique(schedule_np[:, 1]).size
+    invalid_num_state = num_state - 1
+    invalid_num_povm = num_povm - 1
+
+    with pytest.raises(ValueError):
+        _, _ = s_qpt.load_schedule(path, invalid_num_state, num_povm)
+
+    with pytest.raises(ValueError):
+        _, _ = s_qpt.load_schedule(path, num_state, invalid_num_povm)
+
+
+def test_load_schedule_invalid_state_less():
+    test_root_dir = Path(os.path.dirname(__file__)).parent.parent
+    path = test_root_dir / "data/schedule_1qubit_invalid_state_less.csv"
+    schedule_np = np.loadtxt(path, delimiter=",", dtype=np.uint16)
+    num_state = np.unique(schedule_np[:, 0]).size
+    num_povm = np.unique(schedule_np[:, 1]).size
+
+    with pytest.raises(ValueError):
+        _, _ = s_qpt.load_schedule(path, num_state, num_povm)
+
+
+def test_load_schedule_invalid_povm_less():
+    test_root_dir = Path(os.path.dirname(__file__)).parent.parent
+    path = test_root_dir / "data/schedule_1qubit_invalid_povm_less.csv"
+    schedule_np = np.loadtxt(path, delimiter=",", dtype=np.uint16)
+    num_state = np.unique(schedule_np[:, 0]).size
+    num_povm = np.unique(schedule_np[:, 1]).size
+
+    with pytest.raises(ValueError):
+        _, _ = s_qpt.load_schedule(path, num_state, num_povm)
+
+
+def test_load_schedule():
+    test_root_dir = Path(os.path.dirname(__file__)).parent.parent
+    path = test_root_dir / "data/schedule_1qubit_start_from_0.csv"
+    schedule_np = np.loadtxt(path, delimiter=",", dtype=np.uint16)
+    num_state = np.unique(schedule_np[:, 0]).size
+    num_povm = np.unique(schedule_np[:, 1]).size
+
+    expected_num_schedule = schedule_np.shape[0]
+    expected_data = np.array(
+        [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+            [1, 0],
+            [1, 1],
+            [1, 2],
+            [2, 0],
+            [2, 1],
+            [2, 2],
+            [3, 0],
+            [3, 1],
+            [3, 2],
+        ]
+    )
+
+    actual_num_schedule, actual_data = s_qpt.load_schedule(path, num_state, num_povm)
+    assert np.array_equal(actual_num_schedule, expected_num_schedule)
+    assert np.array_equal(actual_data, expected_data)
+
+
 def test_load_weight_list_invalid_num_outcome():
     test_root_dir = Path(os.path.dirname(__file__)).parent.parent
     path = test_root_dir / "data/weight_2valued_uniform.csv"
