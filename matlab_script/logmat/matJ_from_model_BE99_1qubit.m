a2ee617eabe916693e181733cfd0286b7e4c44f8
komo-fr <komo.mdrms@gmail.com>
2020-01-31 13:41:18 +0900

    remove private directory

--- matlab_script/logmat/matJ_from_model_BE99_1qubit.m
+++
@@ -1,9 +0,0 @@
-function output = matJ_from_model_BE99_1qubit(T1, T2, alpha)
-%MATJ_FROM_MODEL_BE99_1QUBIT この関数の概要をここに記述
-%   詳細説明をここに記述
-    vecJ = vecJ_from_model_BE99_1qubit(T1, T2, alpha);   
-    matJ = matJ_from_vecJ_1qubit(vecJ);
-    
-    output = matJ;
-end
-
--- matlab_script/lsqpt/test_matPauliPauliT.m
+++
@@ -1,7 +0,0 @@
-matPauliPauliT = matPauliPauliT_1qubit();
-
-for i = 1:4
-    for j = 1:4
-        matPauliPauliT(i,j).mat
-    end
-end
