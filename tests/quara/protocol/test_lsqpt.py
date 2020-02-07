819882d9d287172f69e9b3a507a7710499562e26
snuffkin <tknstyk@gmail.com>
2020-02-07 09:56:11 +0900

    テストコードを修正。matL0の虚数単位をjに変更

--- tests/quara/protocol/test_lsqpt.py
+++ tests/quara/protocol/test_lsqpt.py
@@ -13,18 +13,18 @@ def test_check_natural_number():
     parameter_name = "dim"
     # valid
     target = 1
-    lsqpt.load_matL0(target, parameter_name)
+    lsqpt.check_natural_number(target, parameter_name)
 
     # invalid
     target = 0
 
     with pytest.raises(ValueError):
-        lsqpt.load_matL0(target, parameter_name)
+        lsqpt.check_natural_number(target, parameter_name)
 
 
 def test_load_matL0_invalid_rows():
     test_root_dir = Path(os.path.dirname(__file__)).parent.parent
-    path = test_root_dir / "data/matL0_1qubit.csv"
+    path = test_root_dir / "data/matL0_1qubit_X90.csv"
     matL0_np = np.loadtxt(path, delimiter=",", dtype=np.complex128)
     dim = 1
 
@@ -34,16 +34,36 @@ def test_load_matL0_invalid_rows():
 
 def test_load_matL0():
     test_root_dir = Path(os.path.dirname(__file__)).parent.parent
-    path = test_root_dir / "data/matL0_1qubit.csv"
+    path = test_root_dir / "data/matL0_1qubit_X90.csv"
     matL0_np = np.loadtxt(path, delimiter=",", dtype=np.complex128)
     dim = int(math.sqrt(matL0_np.shape[0]))
 
     expected_data = np.array(
         [
-            [1.1 + 0.1j, 1.2 + 0.1j, 1.3 + 0.1j, 1.4 + 0.1j],
-            [2.1 + 0.2j, 2.2 + 0.2j, 2.3 + 0.2j, 2.4 + 0.2j],
-            [3.1 + 0.3j, 3.2 + 0.3j, 3.3 + 0.3j, 3.4 + 0.3j],
-            [4.1 + 0.4j, 4.2 + 0.4j, 4.3 + 0.4j, 4.4 + 0.4j],
+            [
+                0.000000000000000e00,
+                0.000000000000000e00 + 7.853981633974483e-01j,
+                0.000000000000000e00 - 7.853981633974483e-01j,
+                0.000000000000000e00,
+            ],
+            [
+                0.000000000000000e00 + 7.853981633974483e-01j,
+                0.000000000000000e00,
+                0.000000000000000e00,
+                0.000000000000000e00 - 7.853981633974483e-01j,
+            ],
+            [
+                0.000000000000000e00 - 7.853981633974483e-01j,
+                0.000000000000000e00,
+                0.000000000000000e00,
+                0.000000000000000e00 + 7.853981633974483e-01j,
+            ],
+            [
+                0.000000000000000e00,
+                0.000000000000000e00 - 7.853981633974483e-01j,
+                0.000000000000000e00 + 7.853981633974483e-01j,
+                0.000000000000000e00,
+            ],
         ]
     )
 
