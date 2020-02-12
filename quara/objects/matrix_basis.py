753dd6ff135fdde72fea0411723e4a5dcbc3b4ad
komo-fr <komo.mdrms@gmail.com>
2020-02-12 14:49:20 +0900

    fix size

--- quara/objects/matrix_basis.py
+++ quara/objects/matrix_basis.py
@@ -19,10 +19,19 @@ class MatrixBasis:
         # エルミート行列になっているかどうかのチェック
         return mutil.is_hermitian(self.array)
 
+    def is_orthogonal(self):
+        # 直交性のチェック
+        return np.equal(self.array @ self.array.T, self.array.T @ self.array)
+
     def to_vect(self) -> VectorizedMatrixBasis:
         # 自分自身をベクトル化したクラスを返す
         return VectorizedMatrixBasis(self)
 
     def size(self):
         # 行列サイズを返す
-        return self.array.size()
+        return self.array.size
+
+    def n(self):
+        # 行列の個数を返す
+        return self.array[0] * self.array[2]  # ?
+
