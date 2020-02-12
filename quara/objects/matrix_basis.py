a9cc7de40de5aa74348547e8e16f69abb8f44c25
komo-fr <komo.mdrms@gmail.com>
2020-02-12 13:21:35 +0900

    add arg of MatrixBasis

--- quara/objects/matrix_basis.py
+++ quara/objects/matrix_basis.py
@@ -3,9 +3,8 @@ import quara.utils.matrix_util as mutil
 
 
 class MatrixBasis:
-    def __init__(self):
-        self.array: np.ndarray = None  # TODO
+    def __init__(self, array):
+        self.array: np.ndarray = array
 
     def is_hermitian(self) -> bool:
         return mutil.is_hermitian(self.array)
-
