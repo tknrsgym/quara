56d7d1344011096954325175a57385394777fc7e
komo-fr <komo.mdrms@gmail.com>
2020-02-14 14:05:26 +0900

    add comment to source code

--- quara/objects/composite_system.py
+++ quara/objects/composite_system.py
@@ -3,5 +3,7 @@ from quara.objects.elemental_system import ElementalSystem
 
 
 class CompositeSystem:
+    """合成系を記述するための＼クラス"""
+
     def __init__(self):
         self.elemental_systems: List[ElementalSystem] = []
