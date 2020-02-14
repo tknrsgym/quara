b3273dc28e7f0b6d498afb9aa5a93ed2e080dc3b
snuffkin <tknstyk@gmail.com>
2020-02-14 15:56:13 +0900

    xx_system、stateを実装中

--- quara/objects/composite_system.py
+++ quara/objects/composite_system.py
@@ -1,7 +1,23 @@
 from typing import List
+from quara.objects import elemental_system
 from quara.objects.elemental_system import ElementalSystem
 
 
 class CompositeSystem:
-    def __init__(self):
-        self.elemental_systems: List[ElementalSystem] = []
+    def __init__(self, systems: List[ElementalSystem]):
+        self._elemental_systems = systems
+        # TODO 合成後の基底をMatrixBasisの形で持っておくこと
+        if len(self._elemental_systems) == 1:
+            self._basis = self._elemental_systems[0].hemirtian_basis
+
+    @property
+    def basis(self):
+        # CompositeSystemのbasisを返す
+        return self._basis
+
+
+def get_with_normalized_pauli_basis() -> CompositeSystem:
+    elem_system = elemental_system.get_with_normalized_pauli_basis()
+    system = CompositeSystem([elem_system])
+    return system
+
--- quara/objects/elemental_system.py
+++ quara/objects/elemental_system.py
@@ -1,15 +1,16 @@
+from quara.objects import matrix_basis
 from quara.objects.matrix_basis import MatrixBasis
 
 
 class ElementalSystem:
-    def __init__(self):
+    def __init__(self, basis: MatrixBasis):
         # system_idを持たせなくても同値ではなく同一インスタンスであるかどうかの判定はisで可能だが、
         # system_idで持たせておくとうっかり同値判定（==）で比較しても異なる値として判断されるので、
         # とりあえず持たせておく
         self._system_id: int = id(self)
         self.dimension: int = None  # TODO
         self._computational_basis: MatrixBasis = None  # TODO
-        self._hemirtian_basis: MatrixBasis = None  # TODO
+        self._hemirtian_basis = basis
 
     @property
     def system_id(self):  # read only
@@ -20,5 +21,11 @@ class ElementalSystem:
         return self._computational_basis
 
     @property
-    def hemirtian_basis_basis(self):  # read only?
+    def hemirtian_basis(self):  # read only?
         return self._hemirtian_basis
+
+
+def get_with_normalized_pauli_basis() -> ElementalSystem:
+    basis = matrix_basis.get_normalized_pauli_basis()
+    system = ElementalSystem(basis)
+    return system
