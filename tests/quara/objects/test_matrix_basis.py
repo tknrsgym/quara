97c8e2c3ecafee21b37ec0f5f731023e8240022a
komo-fr <komo.mdrms@gmail.com>
2020-02-14 15:14:43 +0900

    add test for VectorizedMatrixBasis

--- tests/quara/objects/test_matrix_basis.py
+++ tests/quara/objects/test_matrix_basis.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from quara.objects import matrix_basis
+from quara.objects.matrix_basis import VectorizedMatrixBasis
 
 
 def test_get_comp_basis():
@@ -61,8 +62,30 @@ def test_get_normalized_pauli_basis():
     assert np.all(
         basis[2] == 1 / np.sqrt(2) * np.array([[0, -1j], [1j, 0]], dtype=np.complex128)
     )
-    assert np.all(
-        basis[3] == 1 / np.sqrt(2) * np.array([[1, 0], [0, -1]], dtype=np.complex128)
-    )
+    assert np.all(basis[3] == 1 / np.sqrt(2) * [[1, 0], [0, -1]], dtype=np.complex128)
+
     assert basis.size() == (2, 2)
     assert len(basis) == 4
+
+
+class TestVectorizedMatrixBasis:
+    def test_convert(self):
+        source_basis = matrix_basis.get_pauli_basis()
+        actual_vec_basis = VectorizedMatrixBasis(source_basis)
+        expected_vec_basis = [
+            np.array([1, 0, 0, 1]),
+            np.array([0, 1, 1, 0]),
+            np.array([0, -1j, 1j, 0]),
+            np.array([1, 0, 0, -1]),
+        ]
+        assert len(actual_vec_basis) == len(expected_vec_basis)
+        for i, actual in enumerate(actual_vec_basis):
+            assert np.allclose(actual, expected_vec_basis[i])
+
+        # test len
+        actual = len(actual_vec_basis)
+        assert actual == 4
+
+        actual_org_basis = actual_vec_basis.org_basis
+        for i, actual in enumerate(actual_org_basis):
+            assert np.array_equal(actual, source_basis[i])
