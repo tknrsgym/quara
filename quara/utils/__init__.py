eff1fcddc35f253fd485b281678eb265f39edd17
komo-fr <komo.mdrms@gmail.com>
2020-02-19 13:46:53 +0900

    improve docs for submodule

--- quara/utils/__init__.py
+++ quara/utils/__init__.py
@@ -1,1 +1,1 @@
-"""utility package. for example, the function that returns whether the matrix is Hermitian."""
+"""Utility package. For example, the function that returns whether the matrix is Hermitian."""
