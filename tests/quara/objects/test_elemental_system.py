9b11bb26467b6a1609e16cb622843dadc109a10a
komo-fr <komo.mdrms@gmail.com>
2020-02-21 15:53:32 +0900

    remove eq from ElementalSystem

--- tests/quara/objects/test_elemental_system.py
+++ tests/quara/objects/test_elemental_system.py
@@ -12,6 +12,28 @@ class TestElementalSystem:
 
         assert e1.name == "e1"
 
-        # Test that the name cannot be updated
+        # Test that "name" cannot be updated
         with pytest.raises(AttributeError):
             e1.name = "e2"
+
+    def test_access_systemid(self):
+        m_basis = get_comp_basis()
+        e1 = esys.ElementalSystem("e1", m_basis)
+        actual = e1.system_id
+        expected = id(e1)
+        assert actual == expected
+
+        # Test that "system_id" cannot be updated
+        with pytest.raises(AttributeError):
+            e1.system_id = "new_id"
+
+    def test_access_dim(self):
+        m_basis = get_comp_basis()
+        e1 = esys.ElementalSystem("e1", m_basis)
+        actual = e1.dim
+        expected = m_basis.dim
+        assert actual == expected
+
+        # Test that "dim" cannot be updated
+        with pytest.raises(AttributeError):
+            e1.dim = 100
