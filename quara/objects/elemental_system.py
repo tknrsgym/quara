576efb56c3d840e5fb00b2a82199bedcd0fe3f77
snuffkin <tknstyk@gmail.com>
2020-02-21 10:12:30 +0900

    operatorに関連する処理を作成中

--- quara/objects/elemental_system.py
+++ quara/objects/elemental_system.py
@@ -5,14 +5,19 @@ from quara.objects.matrix_basis import MatrixBasis
 class ElementalSystem:
     """個々の量子系を記述するためのクラス"""
 
-    def __init__(self, basis: MatrixBasis):
+    def __init__(self, name: str, basis: MatrixBasis):
+        self._name: str = name
         # system_idを持たせなくても同値ではなく同一インスタンスであるかどうかの判定はisで可能だが、
         # system_idで持たせておくとうっかり同値判定（==）で比較しても異なる値として判断されるので、
         # とりあえず持たせておく
         self._system_id: int = id(self)
         self._dim: int = basis.dim
-        self._computational_basis: MatrixBasis = None  # TODO
+        # self._computational_basis: MatrixBasis = None  # TODO
         self._hemirtian_basis: MatrixBasis = basis
+
+    @property
+    def name(self):
+        return self.name
 
     @property
     def system_id(self):  # read only
@@ -31,9 +36,14 @@ class ElementalSystem:
     def hemirtian_basis(self):  # read only?
         return self._hemirtian_basis
 
+    def __eq__(self, other):
+        if not isinstance(other, ElementalSystem):
+            return False
+        return self.name == other.name
+
 
 # TODO この関数は不要？
-def get_with_normalized_pauli_basis() -> ElementalSystem:
+def get_with_normalized_pauli_basis(name: str) -> ElementalSystem:
     basis = matrix_basis.get_normalized_pauli_basis()
-    system = ElementalSystem(basis)
+    system = ElementalSystem(name, basis)
     return system
