d92d6327f1463903c8f7885b2bce7aff66ef878f
snuffkin <tknstyk@gmail.com>
2020-02-26 14:52:03 +0900

    典型的なStateを返す関数を実装

--- quara/objects/state.py
+++ quara/objects/state.py
@@ -5,7 +5,8 @@ import numpy as np
 
 import quara.utils.matrix_util as mutil
 from quara.objects.composite_system import CompositeSystem
-from quara.objects.matrix_basis import MatrixBasis
+from quara.objects.elemental_system import ElementalSystem
+from quara.objects.matrix_basis import MatrixBasis, get_normalized_pauli_basis
 
 
 class State:
@@ -34,6 +35,17 @@ class State:
             raise ValueError(
                 f"dim of CompositeSystem must equal dim of vec. dim of CompositeSystem is {self._composite_system.dim}. dim of vec is {self._dim}"
             )
+
+    @property
+    def vec(self):
+        """returns vector.
+        
+        Returns
+        -------
+        np.array
+            vector
+        """
+        return self._vec
 
     @property
     def dim(self):
@@ -117,18 +129,181 @@ class State:
         np.array
             vector representation for ``other_basis``
         """
-        # check length and dim
-        len_basis = len(self._composite_system.basis)
-        assert len_basis == len(other_basis)
-        assert self._composite_system.basis.dim == other_basis.dim
-
-        # "converted vec"_{\alpha} = \sum_{\beta} Tr["other basis"_{\beta}^{\dagger} "self basis"_{\alpha}] "self vec"_{\alpha}
-        representation_matrix = [
-            mutil.inner_product(val1, val2)
-            for val1, val2 in itertools.product(
-                other_basis.basis, self._composite_system.basis.basis
-            )
-        ]
-        rep_mat = np.array(representation_matrix).reshape(len_basis, len_basis)
-        converted_vec = rep_mat @ self._vec
+        converted_vec = convert_vec(
+            self._vec, self._composite_system.basis, other_basis
+        )
         return converted_vec
+
+
+def convert_vec(
+    from_vec: np.array, from_basis: MatrixBasis, to_basis: MatrixBasis
+) -> np.array:
+    """converts vector representation from ``from_basis`` to ``to_basis``.
+    
+    Parameters
+    ----------
+    from_vec : np.array
+        vector representation before converts vector representation
+    from_basis : MatrixBasis
+        basis before converts vector representation
+    to_basis : MatrixBasis
+        basis after converts vector representation
+    
+    Returns
+    -------
+    np.array
+        vector representation after converts vector representation
+        ``dtype`` is ``float64``
+    """
+    # whether length of from_basis equals length of to_basis
+    if len(from_basis) != len(to_basis):
+        raise ValueError(
+            f"length of from_basis must equal length of to_basis. length of from_basis={len(from_basis)}. length of to_basis is {len(to_basis)}"
+        )
+    len_basis = len(from_basis)
+
+    # whether entries of from_vec are real numbers
+    if from_vec.dtype != np.float64:
+        raise ValueError(
+            f"entries of from_vec must be real numbers. dtype of from_vec is {from_vec._vec.dtype}"
+        )
+
+    # whether dim of from_basis equals dim of to_basis
+    if from_basis.dim != to_basis.dim:
+        raise ValueError(
+            f"dim of from_basis must equal dim of to_basis. dim of from_basis={from_basis.dim}. dim of to_basis is {to_basis.dim}"
+        )
+
+    # "converted_vec"_{\alpha} = \sum_{\beta} Tr["to_basis"_{\beta}^{\dagger} "from_basis"_{\alpha}] "from_vec"_{\alpha}
+    representation_matrix = [
+        mutil.inner_product(val1, val2)
+        for val1, val2 in itertools.product(to_basis, from_basis)
+    ]
+    rep_mat = np.array(representation_matrix).reshape(len_basis, len_basis)
+    converted_vec = rep_mat @ from_vec
+    # return converted_vec.real.astype(np.float64)
+    return converted_vec
+
+
+# basis
+def get_X0_1q_with_normalized_pauli_basis(c_sys: CompositeSystem) -> np.array:
+    """returns vec of state ``X_0`` with normalized pauli basis.
+    
+    Parameters
+    ----------
+    c_sys : CompositeSystem
+        CompositeSystem containing state
+    
+    Returns
+    -------
+    np.array
+        vec of state
+    """
+    from_vec = 1 / np.sqrt(2) * np.array([1, 1, 0, 0], dtype=np.float64)
+    from_basis = get_normalized_pauli_basis()
+    to_vec = convert_vec(from_vec, from_basis, c_sys.basis)
+    state = State(c_sys, to_vec.real.astype(np.float64))
+    return state
+
+
+def get_X1_1q_with_normalized_pauli_basis(c_sys: CompositeSystem) -> np.array:
+    """returns vec of state ``X_1`` with normalized pauli basis.
+    
+    Parameters
+    ----------
+    c_sys : CompositeSystem
+        CompositeSystem containing state
+    
+    Returns
+    -------
+    np.array
+        vec of state
+    """
+    from_vec = 1 / np.sqrt(2) * np.array([1, -1, 0, 0], dtype=np.float64)
+    from_basis = get_normalized_pauli_basis()
+    to_vec = convert_vec(from_vec, from_basis, c_sys.basis)
+    state = State(c_sys, to_vec.real.astype(np.float64))
+    return state
+
+
+def get_Y0_1q_with_normalized_pauli_basis(c_sys: CompositeSystem) -> np.array:
+    """returns vec of state ``Y_0`` with normalized pauli basis.
+    
+    Parameters
+    ----------
+    c_sys : CompositeSystem
+        CompositeSystem containing state
+    
+    Returns
+    -------
+    np.array
+        vec of state
+    """
+    from_vec = 1 / np.sqrt(2) * np.array([1, 0, 1, 0], dtype=np.float64)
+    from_basis = get_normalized_pauli_basis()
+    to_vec = convert_vec(from_vec, from_basis, c_sys.basis)
+    state = State(c_sys, to_vec.real.astype(np.float64))
+    return state
+
+
+def get_Y1_1q_with_normalized_pauli_basis(c_sys: CompositeSystem) -> np.array:
+    """returns vec of state ``Y_1`` with normalized pauli basis.
+    
+    Parameters
+    ----------
+    c_sys : CompositeSystem
+        CompositeSystem containing state
+    
+    Returns
+    -------
+    np.array
+        vec of state
+    """
+    from_vec = 1 / np.sqrt(2) * np.array([1, 0, -1, 0], dtype=np.float64)
+    from_basis = get_normalized_pauli_basis()
+    to_vec = convert_vec(from_vec, from_basis, c_sys.basis)
+    state = State(c_sys, to_vec.real.astype(np.float64))
+    return state
+
+
+def get_Z0_1q_with_normalized_pauli_basis(c_sys: CompositeSystem) -> np.array:
+    """returns vec of state ``Z_0`` with normalized pauli basis.
+    
+    Parameters
+    ----------
+    c_sys : CompositeSystem
+        CompositeSystem containing state
+    
+    Returns
+    -------
+    np.array
+        vec of state
+    """
+    from_vec = 1 / np.sqrt(2) * np.array([1, 0, 0, 1], dtype=np.float64)
+    from_basis = get_normalized_pauli_basis()
+    to_vec = convert_vec(from_vec, from_basis, c_sys.basis)
+    state = State(c_sys, to_vec.real.astype(np.float64))
+    return state
+
+
+def get_Z1_1q_with_normalized_pauli_basis(c_sys: CompositeSystem) -> np.array:
+    """returns vec of state ``Z_1`` with normalized pauli basis.
+    
+    Parameters
+    ----------
+    c_sys : CompositeSystem
+        CompositeSystem containing state
+    
+    Returns
+    -------
+    np.array
+        vec of state
+    """
+    from_vec = 1 / np.sqrt(2) * np.array([1, 0, 0, -1], dtype=np.float64)
+    from_basis = get_normalized_pauli_basis()
+    to_vec = convert_vec(from_vec, from_basis, c_sys.basis)
+    state = State(c_sys, to_vec.real.astype(np.float64))
+    return state
+
+
+# TODO 2qubit. \frac{1}{\sqrt{2}}(|00>+|11>)
