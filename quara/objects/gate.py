1e5bfc8a2ec2a4ef8ccefb2a5622634b381d5325
snuffkin <tknstyk@gmail.com>
2020-02-28 09:45:24 +0900

    関数名を小文字化

--- quara/objects/gate.py
+++ quara/objects/gate.py
@@ -9,16 +9,16 @@ import quara.utils.matrix_util as mutil
 
 
 class Gate:
-    def __init__(self, c_sys: CompositeSystem, HS: np.ndarray):
+    def __init__(self, c_sys: CompositeSystem, hs: np.ndarray):
         self._composite_system: CompositeSystem = c_sys
-        self._HS: np.ndarray = HS
+        self._hs: np.ndarray = hs
         # TODO check HS is square matrix
         # TODO check dim of HS is square number
         # TODO check dtype=np.float64
         # TODO check dim of HS equals dim of compsite system
 
     @property
-    def HS(self):
+    def hs(self):
         """returns HS representation of gate.
         
         Returns
@@ -26,7 +26,7 @@ class Gate:
         np.array
             HS representation of gate
         """
-        return self._HS
+        return self._hs
 
     def is_tp(self):
 
@@ -34,25 +34,25 @@ class Gate:
         pass
 
 
-def convert_HS(
-    from_HS: np.array, from_basis: MatrixBasis, to_basis: MatrixBasis
+def convert_hs(
+    from_hs: np.array, from_basis: MatrixBasis, to_basis: MatrixBasis
 ) -> np.array:
     # TODO implement
 
     # TODO parameter check
     representation_matrix = [
         mutil.inner_product(
-            B_alpha.reshape(1, -1)[0], from_HS @ B_beta.reshape(1, -1)[0]
+            B_alpha.reshape(1, -1)[0], from_hs @ B_beta.reshape(1, -1)[0]
         )
         for B_alpha, B_beta in itertools.product(from_basis, to_basis)
     ]
-    HS = np.array(representation_matrix).reshape(
+    hs = np.array(representation_matrix).reshape(
         from_basis.dim ** 2, from_basis.dim ** 2
     )
-    return HS
+    return hs
 
 
-def get_HS(matrix: np.array, basis: MatrixBasis) -> np.array:
+def get_hs(matrix: np.array, basis: MatrixBasis) -> np.array:
     # TODO parameter check
 
     # HS_{\alpha\beta} = <<B_{\alpha}| A(B_{\beta})>>
@@ -62,38 +62,38 @@ def get_HS(matrix: np.array, basis: MatrixBasis) -> np.array:
         )
         for B_alpha, B_beta in itertools.product(basis, basis)
     ]
-    HS = np.array(representation_matrix).reshape(basis.dim ** 2, basis.dim ** 2)
-    return HS
+    hs = np.array(representation_matrix).reshape(basis.dim ** 2, basis.dim ** 2)
+    return hs
 
 
-def get_X(c_sys: CompositeSystem) -> Gate:
+def get_x(c_sys: CompositeSystem) -> Gate:
     # TODO check dim of CompositeSystem = 2
     # convert "HS representation in Pauli basis" to "HS representation in basis of CompositeSystem"
     matrix = np.array(
         [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, -1, 0], [0, 0, 0, -1]], dtype=np.float64
     )
-    HS = convert_HS(matrix, get_normalized_pauli_basis(), c_sys.basis)
-    gate = Gate(c_sys, HS)
+    hs = convert_hs(matrix, get_normalized_pauli_basis(), c_sys.basis)
+    gate = Gate(c_sys, hs)
     return gate
 
 
-def get_Y(c_sys: CompositeSystem) -> Gate:
+def get_y(c_sys: CompositeSystem) -> Gate:
     # TODO check dim of CompositeSystem = 2
     # convert "HS representation in Pauli basis" to "HS representation in basis of CompositeSystem"
     matrix = np.array(
         [[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 1, 0], [0, 0, 0, -1]], dtype=np.float64
     )
-    HS = convert_HS(matrix, get_normalized_pauli_basis(), c_sys.basis)
-    gate = Gate(c_sys, HS)
+    hs = convert_hs(matrix, get_normalized_pauli_basis(), c_sys.basis)
+    gate = Gate(c_sys, hs)
     return gate
 
 
-def get_Z(c_sys: CompositeSystem) -> Gate:
+def get_z(c_sys: CompositeSystem) -> Gate:
     # TODO check dim of CompositeSystem = 2
     # convert "HS representation in Pauli basis" to "HS representation in basis of CompositeSystem"
     matrix = np.array(
         [[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]], dtype=np.float64
     )
-    HS = convert_HS(matrix, get_normalized_pauli_basis(), c_sys.basis)
-    gate = Gate(c_sys, HS)
+    hs = convert_hs(matrix, get_normalized_pauli_basis(), c_sys.basis)
+    gate = Gate(c_sys, hs)
     return gate
