8c1b707b39b99af8de39a93723fe634050e59dc5
komo-fr <komo.mdrms@gmail.com>
2020-04-03 15:17:04 +0900

    add validation for atol of Settings

--- quara/settings.py
+++ quara/settings.py
@@ -1,5 +1,6 @@
 class Settings:
-    __atol = 1e-13
+    __first_default_atol = 1e-13
+    __atol = __first_default_atol
 
     @classmethod
     def get_atol(cls) -> float:
@@ -15,7 +16,7 @@ class Settings:
         return cls.__atol
 
     @classmethod
-    def set_atol(cls, atol: float):
+    def set_atol(cls, atol: float) -> None:
         """updates global setting of ``atol``.
 
         Parameters
@@ -23,4 +24,6 @@ class Settings:
         atol : float
             global setting of ``atol``.
         """
+        if type(atol) != float:
+            raise TypeError("Type of `atol` must be float.")
         cls.__atol = atol
