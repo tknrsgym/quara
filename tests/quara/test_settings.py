83cce1e559faf23e279db0311bdbfb164e6ec4a8
komo-fr <komo.mdrms@gmail.com>
2020-04-03 15:17:04 +0900

    add validation for atol of Settings

--- tests/quara/test_settings.py
+++ tests/quara/test_settings.py
@@ -1,4 +1,13 @@
+import pytest
+
 from quara.settings import Settings
+
+
+@pytest.fixture()
+def fixture_atol():
+    first_default_atol = Settings.get_atol()
+    yield
+    Settings.set_atol(first_default_atol)
 
 
 class TestSettings:
@@ -10,11 +19,17 @@ class TestSettings:
         expected = 1e-13
         assert actual == expected
 
-    def test_atol_setter(self):
+    def test_atol_setter(self, fixture_atol):
         # Arrange & Act
-        Settings.set_atol(1)
+        Settings.set_atol(1.0)
         actual = Settings.get_atol()
 
         # Assert
-        expected = 1
+        expected = 1.0
         assert actual == expected
+
+    def test_atol_setter_validation_ng(self):
+        # Arrange & Act & Assert
+        with pytest.raises(TypeError):
+            # TypeError: Type of `atol` must be float.
+            Settings.set_atol(1)
