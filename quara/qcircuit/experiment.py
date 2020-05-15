45e3e3199f2e45a6eeb948f9d8f69e896dd7f93d
komo-fr <komo.mdrms@gmail.com>
2020-05-15 16:29:20 +0900

    add prototype of Experiment class (no test)

---
+++ quara/qcircuit/experiment.py
@@ -0,0 +1,162 @@
+import collections
+from typing import List, Tuple
+
+from quara.objects.gate import Gate
+from quara.objects.povm import Povm
+from quara.objects.state import State
+
+
+class QuaraScheduleItemError(Exception):
+    pass
+
+
+class QuaraScheduleOrderError(Exception):
+    pass
+
+
+class Experiment:
+    def __init__(
+        self,
+        states: List[State],
+        povms: List[Povm],
+        gates: List[Gate],
+        schedules: List[List[Tuple[str, int]]],
+    ) -> None:
+
+        # TODO: error message
+        if not self._validate_type(states, State):
+            raise TypeError("'states' must be a list of State.")
+
+        if not self._validate_type(povms, Povm):
+            raise TypeError("'povms' must be a list of Povm.")
+
+        if not self._validate_type(gates, Gate):
+            raise TypeError("'gates' must be a list of Gate.")
+
+        # TODO: validate schedules
+
+        # Set
+        self._states: List[State] = states
+        self._povms: List[Povm] = povms
+        self._gates: List[Gate] = gates
+        self._schedules: List[List[Tuple[str, int]]] = schedules
+
+        # TODO:
+        # mprocesses : List[MProcess]
+
+    @property
+    def states(self) -> List[State]:
+        return self._states
+
+    @property
+    def povms(self) -> List[Povm]:
+        return self._povms
+
+    @property
+    def gates(self) -> List[Gate]:
+        return self._gates
+
+    @property
+    def schedules(self) -> List[List[Tuple[str, int]]]:
+        return self._schedules
+
+    # TODO: setter
+
+    def _validate_type(self, targets, expected_type) -> bool:
+        for target in targets:
+            if target and not isinstance(target, expected_type):
+                return False
+        return True
+
+    def _validate_schedules(self, schedules: List[List[Tuple[str, int]]]) -> bool:
+        """
+        - The schedule always starts with the state.
+        - There must be one state.
+        - The gate and mprocess are 0~N
+        - POVM is 0 or 1.
+        - The last one is povm or mprocess
+
+        Parameters
+        ----------
+        schedules : List[List[Tuple[str, int]]]
+            [description]
+
+        Returns
+        -------
+        bool
+            [description]
+        """
+
+        for i, schedule in enumerate(schedules):
+            # 何番目のscheduleで何のエラーが発生したのかわかるようにする
+            try:
+                for item in schedule:
+                    self._validate_schedule_item(item)
+            except (ValueError, IndexError, TypeError):
+                # TODO: error message
+                raise QuaraScheduleItemError
+            try:
+                self._validate_schedule_order(schedule)
+            except ValueError:
+                # TODO: error message
+                raise QuaraScheduleOrderError
+
+    def _validate_schedule_order(self, schedule: List[Tuple[str, int]]):
+        """
+        - schedule単体の並び順に問題がないか検証する
+        - scheduleの最初はstate, 最後はpovmで終わっている。
+
+        Parameters
+        ----------
+        schedule : List[Tuple[str, int]]
+            [description]
+        """
+
+        if len(schedule) < 2:
+            raise ValueError
+
+        TYPE_INDEX = 0
+        INDEX_INDEX = 1
+
+        if schedule[0][TYPE_INDEX] == "state":
+            raise ValueError("scheduleの最初の要素はstateである必要があります")
+        if schedule[-1][TYPE_INDEX] in ["povm", "mprocess"]:
+            raise ValueError("scheduleの最後の要素はpovmかmprocessである必要があります")
+
+        counter = collections.Counter([s[TYPE_INDEX] for s in schedule])
+        if counter["state"] >= 2:
+            raise ValueError("1つのスケジュールでStateは1つである必要があります")
+        if counter["povm"] >= 2:
+            raise ValueError("1つのスケジュールでPovmは1つである必要があります")
+
+    def _validate_schedule_item(self, item: Tuple[str, int]) -> None:
+        # scheduleのtuple単体の中身に問題がないか検証する
+        if len(item) != 2:
+            raise ValueError("Scheduleのitemは、strとintのタプルで表現してください")
+
+        item_name = item[0]
+        item_index = item[1]
+        if type(item_name) != str:
+            raise TypeError("Scheduleのitemは、strとintのタプルで表現してください")
+
+        if type(item_index) != int:
+            raise TypeError("Scheduleのitemは、strとintのタプルで表現してください")
+
+        # TODO: 大文字・小文字の考慮。現状は小文字だけを想定する
+        if item_name not in ["state", "povm", "gate", "mprocess"]:
+            # TODO: error message
+            raise ValueError(
+                "Scheduleのitemではstate, povm, gate, mprocessのいずれかで指定してください。"
+            )
+
+        # TODO: mprocessへの対応
+        name2list = dict(state=self._states, povm=self._povms, gate=self._gates)
+        if not (0 <= item_index < len(name2list[item_name])):
+            # TODO: error message
+            raise IndexError()
+
+    def calc_prob_dist(self):
+        pass
+
+    def calc_prob_dists(self):
+        pass
