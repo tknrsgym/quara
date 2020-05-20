03f86caac01ef1692e18a2d548efbde39ca689a4
komo-fr <komo.mdrms@gmail.com>
2020-05-20 11:35:02 +0900

    [Experiment] add test for the first element of the schedule

--- tests/quara/qcircuit/test_experiment.py
+++ tests/quara/qcircuit/test_experiment.py
@@ -6,18 +6,14 @@ from quara.objects import matrix_basis
 from quara.objects.composite_system import CompositeSystem
 from quara.objects.elemental_system import ElementalSystem
 from quara.objects.gate import Gate, get_h, get_i
-from quara.objects.povm import (
-    Povm,
-    get_x_measurement,
-    get_y_measurement,
-    get_z_measurement,
-)
+from quara.objects.povm import (Povm, get_x_measurement, get_y_measurement,
+                                get_z_measurement)
 from quara.objects.state import State, get_x0_1q, get_y0_1q
-from quara.qcircuit.experiment import Experiment
+from quara.qcircuit.experiment import Experiment, QuaraScheduleOrderError
 
 
 class TestExperiment:
-    def test_property(self):
+    def array_states_povms_gates(self):
         # Array
         e_sys = ElementalSystem(0, matrix_basis.get_comp_basis())
         c_sys = CompositeSystem([e_sys])
@@ -25,6 +21,7 @@ class TestExperiment:
         state_0 = get_x0_1q(c_sys)
         state_1 = get_y0_1q(c_sys)
         state_list = [state_0, state_1]
+
         # POVM
         povm_0 = get_x_measurement(c_sys)
         povm_1 = get_x_measurement(c_sys)
@@ -33,39 +30,44 @@ class TestExperiment:
         gate_0 = get_i(c_sys)
         gate_1 = get_h(c_sys)
         gate_list = [gate_0, gate_1]
+        return state_list, povm_list, gate_list
 
+    def test_property(self):
+        # Array
+        states, povms, gates = self.array_states_povms_gates()
         schedule_list = [
             [("state", 0), ("gate", 0), ("povm", 0)],
             [("state", 1), ("gate", 1), ("povm", 1)],
         ]
 
         # Act & Assert
-        _ = Experiment(
-            states=state_list, povms=povm_list, gates=gate_list, schedules=schedule_list
-        )
+        _ = Experiment(states=states, povms=povms, gates=gates, schedules=schedule_list)
 
         # Arrange
-        state_list = [state_0, None]
-        povm_list = [None, povm_1]
-        gate_list = [gate_0, None]
+        source_states = [states[0], None]
+        source_povms = [None, povms[0]]
+        source_gates = [gates[0], None]
 
         # Act & Assert
         exp = Experiment(
-            states=state_list, povms=povm_list, gates=gate_list, schedules=schedule_list
+            states=source_states,
+            povms=source_povms,
+            gates=source_gates,
+            schedules=schedule_list,
         )
 
         # Assert
-        actual, expected = exp.states, state_list
+        actual, expected = exp.states, source_states
         assert len(actual) == len(expected)
         for a, e in zip(actual, expected):
             assert a == e
 
-        actual, expected = exp.povms, povm_list
+        actual, expected = exp.povms, source_povms
         assert len(actual) == len(expected)
         for a, e in zip(actual, expected):
             assert a == e
 
-        actual, expected = exp.gates, gate_list
+        actual, expected = exp.gates, source_gates
         assert len(actual) == len(expected)
         for a, e in zip(actual, expected):
             assert a == e
@@ -77,22 +79,7 @@ class TestExperiment:
 
     def test_init_unexpected_type(self):
         # Array
-        e_sys = ElementalSystem(0, matrix_basis.get_comp_basis())
-        c_sys = CompositeSystem([e_sys])
-        # State
-        state_0 = get_x0_1q(c_sys)
-        state_1 = get_y0_1q(c_sys)
-        ok_state_list = [state_0, state_1]
-
-        # POVM
-        povm_0 = get_x_measurement(c_sys)
-        povm_1 = get_x_measurement(c_sys)
-        ok_povm_list = [povm_0, povm_1]
-        # Gate
-        gate_0 = get_i(c_sys)
-        gate_1 = get_h(c_sys)
-        ok_gate_list = [gate_0, gate_1]
-
+        ok_states, ok_povms, ok_gates = self.array_states_povms_gates()
         schedule_list = [
             [("state", 0), ("gate", 0), ("povm", 0)],
             [("state", 1), ("gate", 1), ("povm", 1)],
@@ -100,34 +87,72 @@ class TestExperiment:
 
         # Act & Assert
         # Case1: Invalid states
-        ng_state_list = [state_0, povm_0]
+        ng_states = [ok_states[0], ok_povms[0]]
         with pytest.raises(TypeError):
             # TypeError: 'states' must be a list of State.
             _ = Experiment(
-                states=ng_state_list,
-                povms=ok_povm_list,
-                gates=ok_gate_list,
+                states=ng_states,
+                povms=ok_povms,
+                gates=ok_gates,
                 schedules=schedule_list,
             )
 
         # Case2: Invalid povms
-        ng_povm_list = [state_0, povm_0]
+        ng_povms = [ok_states[0], ok_povms[0]]
         with pytest.raises(TypeError):
             # TypeError: 'povms' must be a list of Povm.
             _ = Experiment(
-                states=ok_state_list,
-                povms=ng_povm_list,
-                gates=ok_gate_list,
+                states=ok_states,
+                povms=ng_povms,
+                gates=ok_gates,
                 schedules=schedule_list,
             )
 
         # Case3: Invalid gates
-        ng_gate_list = [gate_0, povm_0]
+        ng_gates = [ok_gates[0], ok_povms[0]]
         with pytest.raises(TypeError):
             # TypeError: 'povms' must be a list of Povm.
             _ = Experiment(
-                states=ok_state_list,
-                povms=ok_povm_list,
-                gates=ng_gate_list,
+                states=ok_states,
+                povms=ok_povms,
+                gates=ng_gates,
                 schedules=schedule_list,
+            )
+
+    def test_expeption_too_short_schedule(self):
+        # Array
+        ok_states, ok_povms, ok_gates = self.array_states_povms_gates()
+        ng_schedule_list = [
+            [("state", 0)],
+            [("state", 1), ("gate", 1), ("povm", 1)],
+        ]
+
+        # Act & Assert
+        with pytest.raises(QuaraScheduleOrderError):
+            # There is a schedule with an invalid order.
+            _ = Experiment(
+                states=ok_states,
+                povms=ok_povms,
+                gates=ok_gates,
+                schedules=ng_schedule_list,
+            )
+
+    def test_expeption_not_start_with_state(self):
+        # Array
+        ok_states, ok_povms, ok_gates = self.array_states_povms_gates()
+        ng_schedule_list = [
+            [("state", 0), ("gate", 0), ("povm", 0)],  # OK
+            [("povm", 0), ("gate", 1), ("povm", 1)],  # NG
+            [("state", 1), ("gate", 1), ("povm", 1)],  # OK
+        ]
+
+        # Act & Assert
+        with pytest.raises(QuaraScheduleOrderError):
+            # There is a schedule with an invalid order.
+            # Detail: The first element of the schedule must be a 'state'.
+            _ = Experiment(
+                states=ok_states,
+                povms=ok_povms,
+                gates=ok_gates,
+                schedules=ng_schedule_list,
             )
