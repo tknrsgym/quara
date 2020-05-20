ea2643c1795aa7986dd008f553ed7cc4a1b6027a
Satoyuki Tsukano <tsukano@acroquest.co.jp>
2020-05-20 16:03:32 +0900

    「経験分布のリストを計算する関数」「経験分布のリストのリストを計算する関数」を追加

--- tests/quara/qcircuit/test_data_generator.py
+++ tests/quara/qcircuit/test_data_generator.py
@@ -27,6 +27,7 @@ def test_generate_data_from_probdist():
     expected = [0, 2, 1, 2, 2, 2, 2, 0, 1, 1]
     assert actual == expected
 
+    ### error cases
     # some probabilities are not positive numbers.
     probdist = np.array([-0.2, 0.5, 0.5], dtype=np.float64)
     with pytest.raises(ValueError):
@@ -60,3 +61,116 @@ def test_generate_dataset_from_list_probdist():
     assert len(actual) == len(expected)
     for a, e in zip(actual, expected):
         assert a == e
+
+    ### error cases
+    # the length of ``probdist_list`` does not equal the length of ``data_num_list``.
+    with pytest.raises(ValueError):
+        data_generator.generate_dataset_from_list_probdist(
+            probdist_list, [10, 20, 30], seed_list
+        )
+
+    # ``seed_list`` is not None and the length of ``probdist_list`` does not equal the length of ``seed_list``.
+    with pytest.raises(ValueError):
+        data_generator.generate_dataset_from_list_probdist(
+            probdist_list, data_num_list, [7, 77, 777]
+        )
+
+
+def test_calc_empidist():
+    # Arrange
+    measurement_num = 2
+    data = [1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1]
+    list_num_sum = [5, 10, 20]
+
+    # Act
+    actual = data_generator.calc_empidist(measurement_num, data, list_num_sum)
+
+    # Assert
+    expected = [
+        np.array([0.4, 0.6], dtype=np.float64),
+        np.array([0.3, 0.7], dtype=np.float64),
+        np.array([0.3, 0.7], dtype=np.float64),
+    ]
+    assert len(actual) == len(expected)
+    for a, e in zip(actual, expected):
+        assert np.all(a == e)
+
+    ### error cases
+    # ``measurement_num`` is not non-negative integer.
+    with pytest.raises(ValueError):
+        data_generator.calc_empidist(-1, data, list_num_sum)
+
+    # there is an element of ``list_num_sum`` that is not less than or equal to length of ``data``.
+    with pytest.raises(ValueError):
+        data_generator.calc_empidist(measurement_num, data, [21])
+    with pytest.raises(ValueError):
+        data_generator.calc_empidist(measurement_num, data, [10, 21])
+
+    # there is an element of ``data`` that is not non-negative and less than ``measurement_num``.
+    with pytest.raises(ValueError):
+        data_generator.calc_empidist(measurement_num, [-1], list_num_sum)
+    with pytest.raises(ValueError):
+        data_generator.calc_empidist(measurement_num, [2], list_num_sum)
+
+    # ``list_num_sum`` is not an increasing sequence.
+    with pytest.raises(ValueError):
+        data_generator.calc_empidist(measurement_num, data, [5, 10, 10])
+
+
+def test_calc_list_empidist():
+    # Arrange
+    list_measurement_num = [3, 2]
+    list_data = [
+        [0, 2, 1, 2, 2, 2, 2, 0, 1, 1],
+        [1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1],
+    ]
+    list_list_num_sum = [
+        [2, 10],
+        [5, 10, 20],
+    ]
+
+    # Act
+    list_actual = data_generator.calc_list_empidist(
+        list_measurement_num, list_data, list_list_num_sum
+    )
+
+    # Assert
+    list_expected = [
+        [
+            np.array([0.5, 0.0, 0.5], dtype=np.float64),
+            np.array([0.2, 0.3, 0.5], dtype=np.float64),
+        ],
+        [
+            np.array([0.4, 0.6], dtype=np.float64),
+            np.array([0.3, 0.7], dtype=np.float64),
+            np.array([0.3, 0.7], dtype=np.float64),
+        ],
+    ]
+    assert len(list_actual) == len(list_expected)
+    for actual, expected in zip(list_actual, list_expected):
+        assert len(actual) == len(expected)
+        for a, e in zip(actual, expected):
+            assert all(a == e)
+
+    ### error cases
+    # the length of ``list_measurement_num`` does not equal the length of ``list_data``.
+    wrong_list_data = [
+        [0, 2, 1, 2, 2, 2, 2, 0, 1, 1],
+        [1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1],
+        [1, 1, 1, 0, 0],
+    ]
+    with pytest.raises(ValueError):
+        data_generator.calc_list_empidist(
+            list_measurement_num, wrong_list_data, list_list_num_sum
+        )
+
+    # the length of ``list_measurement_num`` does not equal the length of ``list_list_num_sum``.
+    wrong_list_list_num_sum = [
+        [2, 10],
+        [5, 10, 20],
+        [1, 2, 3],
+    ]
+    with pytest.raises(ValueError):
+        data_generator.calc_list_empidist(
+            list_measurement_num, list_data, wrong_list_list_num_sum
+        )
