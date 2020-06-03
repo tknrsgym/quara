b35406258a9c1ffdacf13f43a052109435dfe02d
Satoyuki Tsukano <tsukano@acroquest.co.jp>
2020-06-03 17:18:46 +0900

    量子トモグラフィライブラリのQTomographyクラス、Estimatorクラス周りを追加(現時点ではから実装)

---
+++ quara/protocol/qtomography/standard/loss_minimization_estimator.py
@@ -0,0 +1,5 @@
+from quara.protocol.qtomography.standard.standard_qtomography_estimator import StandardQTomographyEstimator
+
+
+class LossMinimizationEstimator(StandardQTomographyEstimator):
+    pass
