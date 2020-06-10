de60927da09ffa0299db5a9bceb7714a92ce4296
komo-fr <komo.mdrms@gmail.com>
2020-06-10 16:00:19 +0900

    improve QOperation

--- quara/objects/qoperation.py
+++ quara/objects/qoperation.py
@@ -2,12 +2,38 @@ from abc import abstractmethod
 
 
 class QOperation:
-    def __init__(self, on_para_eq_constraint: bool = True):
+    def __init__(
+        self,
+        on_para_eq_constraint: bool = True,
+        on_algo_eq_constraint: bool = True,
+        on_algo_ineq_constraint: bool = True,
+        eps_proj_physical: float = 10 ** (-4),
+    ):
+        # Validation
+        if eps_proj_physical < 0:
+            raise ValueError("'eps_proj_physical' must be non-negative.")
+
+        # Set
         self._on_para_eq_constraint: bool = on_para_eq_constraint
+        self._on_algo_eq_constraint: bool = on_algo_eq_constraint
+        self._on_algo_ineq_constraint: bool = on_algo_ineq_constraint
+        self._eps_proj_physical = eps_proj_physical
 
     @property
     def on_para_eq_constraint(self) -> bool:  # read only
         return self._on_para_eq_constraint
+
+    @property
+    def on_algo_eq_constraint(self) -> bool:  # read only
+        return self._on_algo_eq_constraint
+
+    @property
+    def on_algo_eq_constraint(self) -> bool:  # read only
+        return self._on_algo_ineq_constraint
+
+    @property
+    def eps_proj_physical(self) -> float:  # read only
+        return self._eps_proj_physical
 
     @abstractmethod
     def to_var(self):
