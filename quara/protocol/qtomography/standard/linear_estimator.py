257032ae017a5565ff5f036c2153cc398c5f235b
Satoyuki Tsukano <tsukano@acroquest.co.jp>
2020-06-17 17:23:34 +0900

    QST関連を実装中

--- quara/protocol/qtomography/standard/linear_estimator.py
+++ quara/protocol/qtomography/standard/linear_estimator.py
@@ -1,5 +1,16 @@
-from quara.protocol.qtomography.standard.standard_qtomography_estimator import StandardQTomographyEstimator
+import numpy as np
+
+from quara.protocol.qtomography.standard.standard_qtomography import StandardQTomography
+from quara.protocol.qtomography.standard.standard_qtomography_estimator import (
+    StandardQTomographyEstimator,
+)
 
 
 class LinearEstimator(StandardQTomographyEstimator):
-    pass
+    def __init__(self):
+        super().__init__()
+
+    def calc_estimate_var(
+        self, qtomography: StandardQTomography, empi_dists: List[List[float]]
+    ) -> np.array:
+        pass
