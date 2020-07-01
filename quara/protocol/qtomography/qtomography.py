7dcd459b17e92d0397fe60ea89a23e417cc9461d
Satoyuki Tsukano <tsukano@acroquest.co.jp>
2020-07-01 12:04:06 +0900

    QST関連(線形推定)のテスト、docstringを作成

--- quara/protocol/qtomography/qtomography.py
+++ quara/protocol/qtomography/qtomography.py
@@ -1,8 +1,9 @@
 from abc import abstractmethod
-from typing import List
+from typing import List, Tuple
 
 import numpy as np
 
+from quara.objects.qoperation import QOperation
 from quara.objects.qoperations import SetQOperations
 from quara.qcircuit.experiment import Experiment
 
@@ -27,22 +28,123 @@ class QTomography:
 
     @property
     def num_schedules(self) -> int:
+        """returns number of schedules.
+
+        Returns
+        -------
+        int
+            number of schedules.
+        """
         return self._num_schedules
 
     @abstractmethod
+    def is_valid_experiment(self) -> bool:
+        """returns whether the experiment is valid.
+
+        this function must be implemented in the subclass.
+
+        Returns
+        -------
+        bool
+            whether the experiment is valid.
+
+        Raises
+        ------
+        NotImplementedError
+            this function does not be implemented in the subclass.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
     def calc_prob_dist(self) -> List[np.array]:
+        """calculates a probability distribution.
+
+        this function must be implemented in the subclass.
+
+        Returns
+        -------
+        List[np.array]
+            a probability distribution.
+
+        Raises
+        ------
+        NotImplementedError
+            this function does not be implemented in the subclass.
+        """
         raise NotImplementedError()
 
     @abstractmethod
     def calc_prob_dists(self) -> List[List[np.array]]:
+        """calculates probability distributions.
+
+        this function must be implemented in the subclass.
+
+        Returns
+        -------
+        List[List[np.array]]
+            probability distributions.
+
+        Raises
+        ------
+        NotImplementedError
+            this function does not be implemented in the subclass.
+        """
         raise NotImplementedError()
 
     @abstractmethod
-    def generate_dataset(self):
+    def generate_dataset(
+        self, data_nums: List[int], seeds: List[int] = None,
+    ) -> List[List[np.array]]:
+        """Run all the schedules to caluclate the probability distribution and generate random data.
+
+        this function must be implemented in the subclass.
+
+        Parameters
+        ----------
+        data_nums : List[int]
+            A list of the number of data to be generated in each schedule. This parameter should be a list of non-negative integers.
+        seeds : List[int], optional
+            A list of the seeds to be used in each schedule, by default None
+
+        Returns
+        -------
+        List[List[np.array]]
+            Generated dataset.
+
+        Raises
+        ------
+        NotImplementedError
+            this function does not be implemented in the subclass.
+        """
         raise NotImplementedError()
 
     @abstractmethod
-    def generate_datasets(self):
+    def generate_empi_dists(
+        self, qoperation: QOperation, num_sum: int, seed: int = None
+    ) -> List[Tuple[int, np.array]]:
+        """Generate empirical distributions using the data generated from probability distributions of all schedules.
+
+        this function must be implemented in the subclass.
+
+        Parameters
+        ----------
+        qoperation : QOperation
+            QOperation to use to generate the experience distributions.
+        num_sum : int
+            the number of data to use to generate the experience distributions for each schedule.
+        seed : int, optional
+            the seed, by default None
+
+        Returns
+        -------
+        List[Tuple[int, np.array]]
+            A list of tuples for the number of data and experience distributions for each schedules.
+
+        Raises
+        ------
+        NotImplementedError
+            this function does not be implemented in the subclass.
+        """
         raise NotImplementedError()
 
     @abstractmethod
@@ -51,8 +153,4 @@ class QTomography:
 
     @abstractmethod
     def func_prob_dists(self):
-        raise NotImplementedError()
-
-    @abstractmethod
-    def is_valid_experiment(self) -> bool:
         raise NotImplementedError()
--- quara/protocol/qtomography/standard/standard_qtomography_estimator.py
+++ quara/protocol/qtomography/standard/standard_qtomography_estimator.py
@@ -15,6 +15,20 @@ class StandardQTomographyEstimator(Estimator):
     def calc_estimate_qoperation(
         self, qtomography: StandardQTomography, empi_dists: List[Tuple[int, np.array]],
     ) -> QOperation:
+        """calculates estimate QOperation.
+
+        Parameters
+        ----------
+        qtomography : StandardQTomography
+            StandardQTomography to calculates estimate QOperation. 
+        empi_dists : List[Tuple[int, np.array]]
+            empirical distributions to calculates estimate QOperation. 
+
+        Returns
+        -------
+        QOperation
+            estimate QOperation.
+        """
         var = self.calc_estimate_var(qtomography, empi_dists)
         qope = qtomography.convert_var_to_qoperation(var)
         return qope
@@ -23,4 +37,54 @@ class StandardQTomographyEstimator(Estimator):
     def calc_estimate_var(
         self, qtomography: StandardQTomography, empi_dists: List[Tuple[int, np.array]],
     ) -> np.array:
+        """calculates estimate variables.
+
+        this function must be implemented in the subclass.
+
+        Parameters
+        ----------
+        qtomography : StandardQTomography
+            StandardQTomography to calculates estimate variables. 
+        empi_dists : List[Tuple[int, np.array]]
+            empirical distributions to calculates estimate variables. 
+
+        Returns
+        -------
+        np.array
+            estimate variables.
+
+        Raises
+        ------
+        NotImplementedError
+            this function does not be implemented in the subclass.
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def calc_estimate_sequence_var(
+        self,
+        qtomography: StandardQTomography,
+        empi_dists_sequence: List[List[Tuple[int, np.array]]],
+    ) -> np.array:
+        """calculates sequence of estimate variables.
+
+        this function must be implemented in the subclass.
+
+        Parameters
+        ----------
+        qtomography : StandardQTomography
+            StandardQTomography to calculates estimate variables. 
+        empi_dists_sequence : List[List[Tuple[int, np.array]]]
+            sequence of empirical distributions to calculates estimate variables. 
+
+        Returns
+        -------
+        np.array
+            sequence of empirical distributions.
+
+        Raises
+        ------
+        NotImplementedError
+            this function does not be implemented in the subclass.
+        """
         raise NotImplementedError()
