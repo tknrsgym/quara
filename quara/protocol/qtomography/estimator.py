cbd659b52cbfb9f9ea5fa8201dd790f314333db7
Satoyuki Tsukano <tsukano@acroquest.co.jp>
2020-07-29 14:27:39 +0900

    EstimationResult対応

--- quara/protocol/qtomography/estimator.py
+++ quara/protocol/qtomography/estimator.py
@@ -3,14 +3,38 @@ from typing import List
 
 class EstimationResult:
     def __init__(self, computation_times: List[float]):
+        """Constructor
+
+        Parameters
+        ----------
+        computation_times : List[float]
+            computation times for each estimate.
+        """
         self._computation_times: List[float] = computation_times
 
     @property
-    def computation_time(self):
-        return self._computation_times[0]
+    def computation_time(self) -> float:
+        """returns computation time for the estimate.
+
+        Returns
+        -------
+        float
+            computation time for the estimate.
+        """
+        if self._computation_times is None:
+            return None
+        else:
+            return self._computation_times[0]
 
     @property
-    def computation_times(self):
+    def computation_times(self) -> List[float]:
+        """returns computation times for each estimate.
+
+        Returns
+        -------
+        List[float]
+            computation times for each estimate.
+        """
         return self._computation_times
 
 
--- tests/quara/protocol/qtomography/standard/test_standard_qtomography_estimator.py
+++ tests/quara/protocol/qtomography/standard/test_standard_qtomography_estimator.py
@@ -5,62 +5,27 @@ from quara.objects.composite_system import CompositeSystem
 from quara.objects.elemental_system import ElementalSystem
 from quara.objects.matrix_basis import get_normalized_pauli_basis
 from quara.objects.povm import (
-    Povm,
     get_x_measurement,
     get_y_measurement,
     get_z_measurement,
 )
-from quara.objects.state import get_z0_1q
-from quara.protocol.qtomography.standard.linear_estimator import LinearEstimator
 from quara.protocol.qtomography.standard.standard_qst import StandardQst
+from quara.protocol.qtomography.standard.standard_qtomography_estimator import (
+    StandardQTomographyEstimationResult,
+)
 
 
-class TestStandardQTomographyEstimator:
-    def test_calc_estimate_qoperation(self):
+class TestStandardQTomographyEstimationResult:
+    def get_estimation_result(self):
         e_sys = ElementalSystem(0, get_normalized_pauli_basis())
         c_sys = CompositeSystem([e_sys])
-
         povm_x = get_x_measurement(c_sys)
         povm_y = get_y_measurement(c_sys)
         povm_z = get_z_measurement(c_sys)
         povms = [povm_x, povm_y, povm_z]
+        qtomography = StandardQst(povms, on_para_eq_constraint=False)
 
-        qst = StandardQst(povms, on_para_eq_constraint=False)
-
-        est = LinearEstimator()
-        empi_dists = [
-            (10, np.array([0.5, 0.5], dtype=np.float64)),
-            (10, np.array([0.5, 0.5], dtype=np.float64)),
-            (10, np.array([1, 0], dtype=np.float64)),
-        ]
-
-        # is_computation_time_required=True
-        actual = est.calc_estimate_qoperation(
-            qst, empi_dists, is_computation_time_required=True
-        )
-        expected = np.array([1, 0, 0, 1], dtype=np.float64) / np.sqrt(2)
-        npt.assert_almost_equal(actual["estimate"].vec, expected, decimal=15)
-        assert type(actual["computation_time"]) == float
-
-        # is_computation_time_required=False
-        actual = est.calc_estimate_qoperation(qst, empi_dists)
-        expected = np.array([1, 0, 0, 1], dtype=np.float64) / np.sqrt(2)
-        npt.assert_almost_equal(actual["estimate"].vec, expected, decimal=15)
-        assert not "computation_time" in actual
-
-    def test_calc_estimate_sequence_qoperation(self):
-        e_sys = ElementalSystem(0, get_normalized_pauli_basis())
-        c_sys = CompositeSystem([e_sys])
-
-        povm_x = get_x_measurement(c_sys)
-        povm_y = get_y_measurement(c_sys)
-        povm_z = get_z_measurement(c_sys)
-        povms = [povm_x, povm_y, povm_z]
-
-        qst = StandardQst(povms, on_para_eq_constraint=False)
-
-        est = LinearEstimator()
-        empi_dists_sequence = [
+        data = [
             [
                 (10, np.array([0.5, 0.5], dtype=np.float64)),
                 (10, np.array([0.5, 0.5], dtype=np.float64)),
@@ -73,20 +38,46 @@ class TestStandardQTomographyEstimator:
             ],
         ]
 
-        # is_computation_time_required=True
-        actual = est.calc_estimate_sequence_qoperation(
-            qst, empi_dists_sequence, is_computation_time_required=True
-        )
-        expected = np.array([1, 0, 0, 1], dtype=np.float64) / np.sqrt(2)
-        for a in actual["estimate"]:
-            npt.assert_almost_equal(a.vec, expected, decimal=15)
-        assert len(actual["computation_time"]) == 2
-        for a in actual["computation_time"]:
-            assert type(a) == float
+        estimated_var_sequence = [
+            np.array([1, 0, 0, 1], dtype=np.float64) / np.sqrt(2),
+            np.array([1, 0, 0, 1], dtype=np.float64) / np.sqrt(2),
+        ]
+        computation_times = [0.1, 0.2]
 
-        # is_computation_time_required=False
-        actual = est.calc_estimate_sequence_qoperation(qst, empi_dists_sequence)
+        result = StandardQTomographyEstimationResult(
+            qtomography, data, estimated_var_sequence, computation_times
+        )
+
+        return result
+
+    def test_access_estimated_var(self):
+        result = self.get_estimation_result()
+        actual = result.estimated_var
         expected = np.array([1, 0, 0, 1], dtype=np.float64) / np.sqrt(2)
-        for a in actual["estimate"]:
-            npt.assert_almost_equal(a.vec, expected, decimal=15)
-        assert not "computation_time" in actual
+        npt.assert_almost_equal(actual, expected, decimal=15)
+
+    def test_access_estimated_var_sequence(self):
+        result = self.get_estimation_result()
+        actual = result.estimated_var_sequence
+        expected = [
+            np.array([1, 0, 0, 1], dtype=np.float64) / np.sqrt(2),
+            np.array([1, 0, 0, 1], dtype=np.float64) / np.sqrt(2),
+        ]
+        for a, e in zip(actual, expected):
+            npt.assert_almost_equal(a, e, decimal=15)
+
+    def test_access_estimated_qoperation(self):
+        result = self.get_estimation_result()
+        actual = result.estimated_qoperation
+        expected = np.array([1, 0, 0, 1], dtype=np.float64) / np.sqrt(2)
+        npt.assert_almost_equal(actual.to_stacked_vector(), expected, decimal=15)
+
+    def test_access_estimated_qoperation_sequence(self):
+        result = self.get_estimation_result()
+        actual = result.estimated_qoperation_sequence
+        expected = [
+            np.array([1, 0, 0, 1], dtype=np.float64) / np.sqrt(2),
+            np.array([1, 0, 0, 1], dtype=np.float64) / np.sqrt(2),
+        ]
+        for a, e in zip(actual, expected):
+            npt.assert_almost_equal(a.to_stacked_vector(), e, decimal=15)
