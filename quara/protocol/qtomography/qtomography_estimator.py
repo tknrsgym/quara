3f0c87900b9437b95a77fd3724bd066cab03ac45
Satoyuki Tsukano <tsukano@acroquest.co.jp>
2020-08-26 13:35:45 +0900

    QTomographyEstimatorを追加

---
+++ quara/protocol/qtomography/qtomography_estimator.py
@@ -0,0 +1,9 @@
+from quara.protocol.qtomography.estimator import Estimator, EstimationResult
+
+
+class QTomographyEstimationResult(EstimationResult):
+    pass
+
+
+class QTomographyEstimator(Estimator):
+    pass
