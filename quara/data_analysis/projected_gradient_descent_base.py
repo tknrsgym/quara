c0795e4d848a93e3e21b0f18503ed3f530c08e92
Satoyuki Tsukano <tknstyk@gmail.com>
2020-09-16 16:24:16 +0900

    pgdbのテストケースを追加

--- quara/data_analysis/projected_gradient_descent_base.py
+++ quara/data_analysis/projected_gradient_descent_base.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import numpy as np
 
 
@@ -11,7 +13,7 @@ from quara.data_analysis.minimization_algorithm import (
 class ProjectedGradientDescentBaseOption(MinimizationAlgorithmOption):
     def __init__(
         self,
-        func_proj,
+        func_proj: Callable[[np.array], np.array],
         var_start: np.array,
         mu: float = None,
         gamma: float = 0.3,
@@ -22,7 +24,7 @@ class ProjectedGradientDescentBaseOption(MinimizationAlgorithmOption):
             var_start, True, False, on_iteration_history=on_iteration_history
         )
 
-        self._func_proj = func_proj
+        self._func_proj: Callable[[np.array], np.array] = func_proj
 
         if mu is None:
             mu = 3 / (2 * np.sqrt(var_start.shape[0]))
@@ -78,14 +80,12 @@ class ProjectedGradientDescentBase(MinimizationAlgorithm):
                 x_prev = x_next
 
             y_prev = (
-                algorithm_option.func_proj(
-                    x_prev - loss_function.gradient(x_prev) / mu, loss_function_option
-                )
+                algorithm_option.func_proj(x_prev - loss_function.gradient(x_prev) / mu)
                 - x_prev
             )
-            print(
-                f"k={k}, y_prev={y_prev}, x_prev={x_prev}, gradient(x_prev)={loss_function.gradient(x_prev)}, mu={mu}"
-            )
+            # print(
+            #    f"k={k}, y_prev={y_prev}, x_prev={x_prev}, gradient(x_prev)={loss_function.gradient(x_prev)}, mu={mu}"
+            # )
 
             alpha = 1.0
             while self._is_doing_for_alpha(x_prev, y_prev, alpha, gamma, loss_function):
@@ -101,12 +101,10 @@ class ProjectedGradientDescentBase(MinimizationAlgorithm):
             #    f"k={k}, val={val}, grad(x_prev)={loss_function.gradient(x_prev)}, grad(x_next)={loss_function.gradient(x_next)}"
             # )
             is_doing = True if val > eps else False
-            if k == 10:
-                break
 
-        print(
-            f"var_ref={loss_function._var_ref}, var_a={loss_function_option.var_a}: {algorithm_option.var_start} -> {x_next}"
-        )
+        # print(
+        #    f"var_ref={loss_function._var_ref}, var_a={loss_function_option.var_a}: {algorithm_option.var_start} -> {x_next}"
+        # )
         return x_next
 
     def _is_doing_for_alpha(
@@ -121,4 +119,5 @@ class ProjectedGradientDescentBase(MinimizationAlgorithm):
         right_side = loss_function.value(x_prev) + gamma * alpha * (
             np.dot(y_prev, loss_function.gradient(x_prev))
         )
+        # print(f"  left_side={left_side}, right_side={right_side}")
         return left_side > right_side
