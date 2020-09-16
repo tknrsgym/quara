08a62105118b5eccec4f0ebd57e1f11ab364512a
Satoyuki Tsukano <tknstyk@gmail.com>
2020-09-16 17:21:58 +0900

    on_iteration_historyの対応を実装中

--- quara/data_analysis/projected_gradient_descent_base.py
+++ quara/data_analysis/projected_gradient_descent_base.py
@@ -58,19 +58,31 @@ class ProjectedGradientDescentBase(MinimizationAlgorithm):
         loss_function: LossFunction,
         loss_function_option: LossFunctionOption,
         algorithm_option: ProjectedGradientDescentBaseOption,
+        on_iteration_history: bool = False,
     ) -> np.array:
-        # TODO loss_function, algorithm_option is_gradient_requiredとかのチェック
         # TODO history対応
+
+        if loss_function.on_gradient == False:
+            raise ValueError(
+                "to execute ProjectedGradientDescentBase, 'on_gradient' of loss_function must be True."
+            )
+        if algorithm_option.is_gradient_required == False:
+            raise ValueError(
+                "to execute ProjectedGradientDescentBase, 'is_gradient_required' of algorithm option must be True."
+            )
 
         x_prev = algorithm_option.var_start
         x_next = None
         mu = algorithm_option.mu
         gamma = algorithm_option.gamma
         eps = algorithm_option.eps
-        # print(
-        #    f"x_start={x_prev}, var_ref={loss_function._var_ref}, var_a={loss_function_option.var_a}, mu={mu}, gamma={gamma}, eps={eps}"
-        # )
-        # print(f"grad(x_start)={loss_function.gradient(x_prev)}")
+
+        # variables for debug
+        if on_iteration_history:
+            values = [loss_function.value(x_prev)]
+            xs = [x_prev]
+            ys = []
+            alphas = []
 
         k = 0
         is_doing = True
@@ -83,29 +95,35 @@ class ProjectedGradientDescentBase(MinimizationAlgorithm):
                 algorithm_option.func_proj(x_prev - loss_function.gradient(x_prev) / mu)
                 - x_prev
             )
-            # print(
-            #    f"k={k}, y_prev={y_prev}, x_prev={x_prev}, gradient(x_prev)={loss_function.gradient(x_prev)}, mu={mu}"
-            # )
 
             alpha = 1.0
             while self._is_doing_for_alpha(x_prev, y_prev, alpha, gamma, loss_function):
                 alpha = 0.5 * alpha
-                # print(f"k={k}, alpha={alpha}")
 
             x_next = x_prev + alpha * y_prev
-            # print(f"k={k}, x_prev={x_prev}, x_next={x_next}")
             k += 1
 
             val = loss_function.value(x_prev) - loss_function.value(x_next)
-            # print(
-            #    f"k={k}, val={val}, grad(x_prev)={loss_function.gradient(x_prev)}, grad(x_next)={loss_function.gradient(x_next)}"
-            # )
             is_doing = True if val > eps else False
 
-        # print(
-        #    f"var_ref={loss_function._var_ref}, var_a={loss_function_option.var_a}: {algorithm_option.var_start} -> {x_next}"
-        # )
-        return x_next
+            # variables for debug
+            if on_iteration_history:
+                values.append(loss_function.value(x_next))
+                xs.append(x_next)
+                ys.append(y_prev)
+                alphas.append(alpha)
+
+        if on_iteration_history:
+            history = {
+                "k": k,
+                "value": values,
+                "x": xs,
+                "y": ys,
+                "alpha": alphas,
+            }
+            return x_next, history
+        else:
+            return x_next
 
     def _is_doing_for_alpha(
         self,
@@ -119,5 +137,4 @@ class ProjectedGradientDescentBase(MinimizationAlgorithm):
         right_side = loss_function.value(x_prev) + gamma * alpha * (
             np.dot(y_prev, loss_function.gradient(x_prev))
         )
-        # print(f"  left_side={left_side}, right_side={right_side}")
         return left_side > right_side
