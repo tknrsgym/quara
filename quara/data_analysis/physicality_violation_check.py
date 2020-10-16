4872d474222565fa709f6078fa1c43ca9ec9ba9c
komo-fr <komo.mdrms@gmail.com>
2020-10-16 15:57:59 +0900

    add prototype for report (on_para_eq_constraint=True)

--- quara/data_analysis/physicality_violation_check.py
+++ quara/data_analysis/physicality_violation_check.py
@@ -59,16 +59,20 @@ def get_physicality_violation_result_for_state_affine(
     return value_list
 
 
+# TODO: rename
 def check_physicality_violation(
-    estimation_results: List[EstimationResult],
+    estimation_results: List[EstimationResult], num_data_index: int = 0
 ) -> Dict[str, Any]:
     qoperation = estimation_results[0].estimated_qoperation
-    estimated_qoperations = [qo.estimated_qoperation for qo in estimation_results]
+    estimated_qoperations = [
+        result.estimated_qoperation_sequence[num_data_index]
+        for result in estimation_results
+    ]
     if type(qoperation) == State:
         result = _check_physicality_violation_for_state(estimated_qoperations)
     else:
         # TODO: error message
-        raise ValueError()
+        raise NotImplementedError()
     return result
 
 
@@ -102,6 +106,7 @@ def _check_physicality_violation_for_state(
 def make_prob_dist_histogram(
     values: List[float],
     bin_size: int,
+    num_data: int,
     x_range: Optional[tuple] = None,
     annotation_vlines: List[Union[float, int]] = None,
 ):
@@ -132,6 +137,9 @@ def make_prob_dist_histogram(
             )
 
     fig.update_yaxes(range=[0, 1], tickvals=ytickvals, title="Frequency")
+    n_rep = len(values)
+    title = f"N={num_data}, Nrep={n_rep}"
+    fig.update_layout(title=title)
 
     return fig
 
@@ -174,19 +182,36 @@ def _convert_result_to_qoperation(
     return estimated_qoperations
 
 
+# Qst, on_para_eq_constraint=True
+def make_graph_trace(
+    estimation_results: List[EstimationResult],
+    num_data: List[int],
+    num_data_index: int = 0,
+    bin_size: float = 0.0001,
+) -> "Figure":
+    violation_result = check_physicality_violation(
+        estimation_results, num_data_index=num_data_index
+    )
+    num_data = num_data[num_data_index]
+    fig = make_prob_dist_histogram(
+        violation_result["trace_list"], num_data=num_data, bin_size=bin_size
+    )
+    return fig
+
+
 # common, on_para_eq_constraint=False
 def make_graphs_eigenvalues(
     estimation_results: List[EstimationResult],
     true_object: "QOperation",
     num_data: List[int],
-    index: int = 0,
+    num_data_index: int = 0,
     bin_size: float = 0.0001,
 ) -> List["Figure"]:
     estimated_qoperations = _convert_result_to_qoperation(
-        estimation_results, index=index
+        estimation_results, index=num_data_index
     )
 
-    n_data = num_data[index]
+    n_data = num_data[num_data_index]
     if type(true_object) == State:
         figs = _make_graphs_eigenvalues_state(
             estimated_qoperations, true_object, n_data, bin_size
@@ -236,7 +261,7 @@ def _make_graphs_eigenvalues_state(
     figs = []
     for i, values in enumerate(sorted_eigenvalues_list):
         # plot eigenvalues of estimated states
-        fig = make_prob_dist_histogram(values, bin_size=bin_size)
+        fig = make_prob_dist_histogram(values, num_data=num_data, bin_size=bin_size)
         fig.update_layout(title=f"N={num_data}, Nrep={len(values)}")
         fig.update_xaxes(title=f"Eigenvalue (i={i})")
 
@@ -273,7 +298,7 @@ def _make_graphs_sum_unphysical_eigenvalues_state(
     figs = []
     # Figure 1
     fig = make_prob_dist_histogram(
-        less_than_zero_list, bin_size=bin_size, annotation_vlines=[0]
+        less_than_zero_list, bin_size=bin_size, num_data=num_data, annotation_vlines=[0]
     )
 
     n_unphysical = len(less_than_zero_list)
@@ -284,7 +309,10 @@ def _make_graphs_sum_unphysical_eigenvalues_state(
 
     # Figure 2
     fig = make_prob_dist_histogram(
-        greater_than_one_list, bin_size=bin_size, annotation_vlines=[1]
+        greater_than_one_list,
+        bin_size=bin_size,
+        num_data=num_data,
+        annotation_vlines=[1],
     )
 
     n_unphysical = len(less_than_zero_list)
