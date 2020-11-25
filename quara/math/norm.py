c6ceeb8286a96f0e3c8a9e48408b0f58a117ecce
Satoyuki Tsukano <tknstyk@gmail.com>
2020-11-25 10:17:20 +0900

    関数コメントを追加

--- quara/math/norm.py
+++ quara/math/norm.py
@@ -2,5 +2,19 @@ import numpy as np
 
 
 def l2_norm(x: np.array, y: np.array) -> np.float64:
+    """calculates L2 norm.
+
+    Parameters
+    ----------
+    x : np.array
+        vector.(1-dim numpy array)
+    y : np.array
+        vector.(1-dim numpy array)
+
+    Returns
+    -------
+    np.float64
+        L^2 norm of x and y.
+    """
     norm = np.linalg.norm(x - y)
     return norm
