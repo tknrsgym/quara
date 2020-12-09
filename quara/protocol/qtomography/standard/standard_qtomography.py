b654ff2bd4023d53cfdee525bc94c6ba82346f01
Satoyuki Tsukano <tknstyk@gmail.com>
2020-12-09 15:18:27 +0900

    Cramer-Rao boundを計算する関数を実装

--- quara/protocol/qtomography/standard/standard_qtomography.py
+++ quara/protocol/qtomography/standard/standard_qtomography.py
@@ -306,6 +306,20 @@ class StandardQTomography(QTomography):
         return mse_total
 
     def calc_fisher_matrix(self, j: int, var: Union[QOperation, np.array]) -> np.array:
+        """calculates Fisher matrix of one schedule.
+
+        Parameters
+        ----------
+        j : int
+            schedule_index
+        var : Union[QOperation, np.array]
+            variables to calculate Fisher matrix of one schedule.
+
+        Returns
+        -------
+        np.array
+            Fisher matrix of one schedule.
+        """
         if isinstance(var, QOperation):
             var = var.to_var()
 
@@ -324,6 +338,20 @@ class StandardQTomography(QTomography):
     def calc_fisher_matrix_total(
         self, var: Union[QOperation, np.array], weights: List[float]
     ) -> np.array:
+        """calculates Fisher matrix of the total schedule.
+
+        Parameters
+        ----------
+        var : Union[QOperation, np.array]
+            variables to calculate Fisher matrix of one schedule.
+        weights : List[float]
+            weights to calculate Fisher matrix of one schedule.
+
+        Returns
+        -------
+        np.array
+            Fisher matrix of the total schedule.
+        """
         fisher_matrices = []
         for schedule_index in range(self.num_schedules):
             fisher_matrices.append(
@@ -334,6 +362,22 @@ class StandardQTomography(QTomography):
     def calc_cramer_rao_bound(
         self, var: Union[QOperation, np.array], N: int, list_N: List[int]
     ) -> np.array:
+        """calculates Cramer-Rao bound.
+
+        Parameters
+        ----------
+        var : Union[QOperation, np.array]
+            variables to calculate Cramer-Rao bound.
+        N : int
+            representative value of the number of data.
+        list_N : List[int]
+            the number of data for each schedule.
+
+        Returns
+        -------
+        np.array
+            Cramer-Rao bound.
+        """
         fisher = self.calc_fisher_matrix_total(var, list_N)
         val = np.trace(np.linalg.inv(fisher)) / N
         return val
