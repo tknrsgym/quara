155d3e096ab0e97772a246ede53b98a5b506b7fb
Satoyuki Tsukano <tknstyk@gmail.com>
2020-12-09 17:23:00 +0900

    POVMTのCramer-Rao boundを実装中

--- tests/quara/protocol/qtomography/standard/test_standard_povmt.py
+++ tests/quara/protocol/qtomography/standard/test_standard_povmt.py
@@ -292,3 +292,57 @@ class TestStandardPovmt:
 
         # Assert
         npt.assert_almost_equal(actual, 0.04, decimal=15)
+
+    def test_calc_cramer_rao_bound(self):
+        # setup system
+        e_sys = ElementalSystem(0, get_normalized_pauli_basis())
+        c_sys = CompositeSystem([e_sys])
+
+        # |+><+|
+        state_x0 = get_x0_1q(c_sys)
+        # |+i><+i|
+        state_y0 = get_y0_1q(c_sys)
+        # |0><0|
+        state_z0 = get_z0_1q(c_sys)
+        # |1><1|
+        state_z1 = get_z1_1q(c_sys)
+        tester_objects = [state_x0, state_y0, state_z0, state_z1]
+
+        # true_object: origin
+        a0, a1, a2, a3 = 1, 0, 0, 0
+        m1 = (1 / np.sqrt(2)) * np.array([a0, a1, a2, a3])
+        m2 = (1 / np.sqrt(2)) * np.array([2 - a0, -a1, -a2, -a3])
+
+        """
+        ### Case 1: on_par_eq_constraint = True
+        # Arange
+        true_object = Povm(vecs=[m1, m2], c_sys=c_sys, on_para_eq_constraint=True)
+        measurement_n = len(true_object.vecs)
+        povmt = StandardPovmt(
+            tester_objects, measurement_n=measurement_n, on_para_eq_constraint=True
+        )
+
+        # Act
+        actual = povmt.calc_cramer_rao_bound(
+            true_object, 100, [1] * len(tester_objects)
+        )
+
+        # Assert
+        npt.assert_almost_equal(actual, 0.02, decimal=15)
+        """
+
+        ### Case 2: on_par_eq_constraint = False
+        # Arange
+        true_object = Povm(vecs=[m1, m2], c_sys=c_sys, on_para_eq_constraint=False)
+        measurement_n = len(true_object.vecs)
+        povmt = StandardPovmt(
+            tester_objects, measurement_n=measurement_n, on_para_eq_constraint=False
+        )
+
+        # Act
+        actual = povmt.calc_cramer_rao_bound(
+            true_object, 100, [1] * len(tester_objects)
+        )
+
+        # Assert
+        npt.assert_almost_equal(actual, 0.08, decimal=15)
