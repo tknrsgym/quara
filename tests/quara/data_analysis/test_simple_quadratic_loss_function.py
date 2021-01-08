a700c44d8d4347d4ab70977d5d67256ad7ff2a41
Satoyuki Tsukano <tknstyk@gmail.com>
2021-01-08 17:30:38 +0900

    クラス名を変更

--- tests/quara/data_analysis/test_simple_quadratic_loss_function.py
+++ tests/quara/data_analysis/test_simple_quadratic_loss_function.py
@@ -4,39 +4,41 @@ import numpy as np
 import numpy.testing as npt
 import pytest
 
-from quara.data_analysis.quadratic_loss_function import QuadraticLossFunction
+from quara.data_analysis.simple_quadratic_loss_function import (
+    SimpleQuadraticLossFunction,
+)
 
 
-class TestQuadraticLossFunction:
+class TestSimpleQuadraticLossFunction:
     def test_update_on_value_true(self):
         var_ref = np.array([1, 2, 3, 4], dtype=np.float64)
-        loss_func = QuadraticLossFunction(var_ref)
+        loss_func = SimpleQuadraticLossFunction(var_ref)
         loss_func._on_value = False
         loss_func._update_on_value_true()
         assert loss_func.on_value == True
 
     def test_update_on_gradient_true(self):
         var_ref = np.array([1, 2, 3, 4], dtype=np.float64)
-        loss_func = QuadraticLossFunction(var_ref)
+        loss_func = SimpleQuadraticLossFunction(var_ref)
         loss_func._on_gradient = False
         loss_func._update_on_gradient_true()
         assert loss_func.on_gradient == True
 
     def test_update_on_hessian_true(self):
         var_ref = np.array([1, 2, 3, 4], dtype=np.float64)
-        loss_func = QuadraticLossFunction(var_ref)
+        loss_func = SimpleQuadraticLossFunction(var_ref)
         loss_func._on_hessian = False
         loss_func._update_on_hessian_true()
         assert loss_func.on_hessian == True
 
     def test_is_option_sufficient(self):
         var_ref = np.array([1, 2, 3, 4], dtype=np.float64)
-        loss_func = QuadraticLossFunction(var_ref)
+        loss_func = SimpleQuadraticLossFunction(var_ref)
         assert loss_func.is_option_sufficient() == True
 
     def test_value(self):
         var_ref = np.array([1, 2, 3, 4], dtype=np.float64)
-        loss_func = QuadraticLossFunction(var_ref)
+        loss_func = SimpleQuadraticLossFunction(var_ref)
         assert loss_func.on_value == True
 
         # Case1: var = var_ref
@@ -55,7 +57,7 @@ class TestQuadraticLossFunction:
 
     def test_gradient(self):
         var_ref = np.array([1, 2, 3, 4], dtype=np.float64)
-        loss_func = QuadraticLossFunction(var_ref)
+        loss_func = SimpleQuadraticLossFunction(var_ref)
         assert loss_func.on_gradient == True
 
         # Case1: var = var_ref
@@ -76,7 +78,7 @@ class TestQuadraticLossFunction:
 
     def test_hessian(self):
         var_ref = np.array([1, 2, 3, 4], dtype=np.float64)
-        loss_func = QuadraticLossFunction(var_ref)
+        loss_func = SimpleQuadraticLossFunction(var_ref)
         assert loss_func.on_hessian == True
 
         # Case1: var = var_ref
