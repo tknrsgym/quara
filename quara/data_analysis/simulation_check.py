6d2b8c490fe9450d1b6ee15a75af82814060ef65
komo-fr <komo.mdrms@gmail.com>
2021-01-27 16:11:56 +0900

    add consistency check to StandardQTomographySimulationCheck class

---
+++ quara/data_analysis/simulation_check.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from quara.data_analysis.simulation import StandardQTomographySimulationSetting
+from quara.data_analysis.consistency_check import execute_consistency_check
+
+
+class StandardQTomographySimulationCheck:
+    def __init__(
+        self,
+        simulation_setting: StandardQTomographySimulationSetting,
+        estimation_results: List["EstimationResult"],
+    ) -> None:
+        self.simulation_setting = simulation_setting
+        self.estimation_results = estimation_results
+
+    def execute_mse_of_empirical_distribution_check(
+        self, eps: float = None, show_detail: bool = True
+    ) -> bool:
+        result = execute_consistency_check(
+            simulation_setting=self.simulation_setting,
+            estimation_results=self.estimation_results,
+            eps=eps,
+            show_detail=show_detail,
+        )
+        return result
+
+    def execute_consistency_check(self):
+        pass
+
+    def execute_mse_of_estimators_check(self):
+        pass
+
+    def execute_physicality_violation_check(self):
+        pass
