52f474a3ead2462f7df2ade65bf4abed8179fa4d
komo-fr <komo.mdrms@gmail.com>
2021-02-03 14:46:32 +0900

    add is_eq_constraint_satisfied/is_ineq_constraint_satisfied to QOeprations(State/Povm/Gate).

--- quara/objects/state.py
+++ quara/objects/state.py
@@ -128,34 +128,11 @@ class State(QOperation):
         """
         return self._dim
 
-    def is_physical(
-        self, atol_eq_const: float = None, atol_ineq_const: float = None
-    ) -> bool:
-        """returns whether the state is physically correct.
+    def is_eq_constraint_satisfied(self, atol: float = None):
+        return self.is_trace_one(atol)
 
-        all of the following conditions are ``True``, the state is physically correct:
-
-        - trace of density matrix equals 1.
-        - density matrix is Hermitian.
-        - density matrix is positive semidefinite.
-
-        Parameters
-        ----------
-        atol_eq_const : float, optional
-            Error tolerance used to determine if the trace of the density matrix is equal to 1. The absolute tolerance parameter, uses :func:`~quara.settings.Settings.get_atol` by default.
-        atol_ineq_const : float, optional
-            Error tolerance used to determine if the density matrix is Hermitian and positive semidefinite. The absolute tolerance parameter, uses :func:`~quara.settings.Settings.get_atol` by default.
-
-        Returns
-        -------
-        bool
-            whether the state is physically correct.
-        """
-        # in `is_positive_semidefinite` function, the state is checked whether it is Hermitian.
-        # therefore, do not call the `is_hermitian` function explicitly.
-        return self.is_trace_one(atol_eq_const) and self.is_positive_semidefinite(
-            atol_ineq_const
-        )
+    def is_ineq_constraint_satisfied(self, atol: float = None):
+        return self.is_positive_semidefinite(atol)
 
     def set_zero(self):
         """sets parameters to zero.
