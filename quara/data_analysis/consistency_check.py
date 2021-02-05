15b7f7b91506f26fed9ab5d8cc86d8fe8885aba7
komo-fr <komo.mdrms@gmail.com>
2021-02-05 17:10:41 +0900

    modify detail log of consistency check

--- quara/data_analysis/consistency_check.py
+++ quara/data_analysis/consistency_check.py
@@ -59,6 +59,7 @@ def execute_consistency_check(
     )
 
     if show_detail:
+        print(f"[{'OK' if result < eps else 'NG'}] Consistency Check")
         print(f"result={result}")
         print(f"eps={eps}")
         print(f"result < eps: {result < eps}")
