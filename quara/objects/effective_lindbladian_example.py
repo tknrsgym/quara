d065ece86f672f5fc2b88ca85156787acbd59de9
Takanori Sugiyama <takanori@mbp20.local>
2021-02-19 18:12:58 +0900

    implemented functions related to the identity gate

---
+++ quara/objects/effective_lindbladian_example.py
@@ -0,0 +1,106 @@
+import numpy
+
+from quara.objects.effective_lindbladian import EffectiveLindbladian
+
+# Identity gate
+
+
+def generate_matrix_identity_gate_hamiltonian(dim: int) -> np.array:
+    """Return Hamiltonian for an identity gate.
+
+    The result is the dim times dim complex zero matrix.
+
+    Parameters
+    ----------
+    dim : int
+        The dimension of the quantum system on which the gate acts.
+
+    Returns
+    ----------
+    np.array
+        The Hamiltonian, which is a complex matrix.
+    """
+    mat = np.zeros((dim, dim), dtype=np.complex128)
+    return mat
+
+
+def generate_matrix_identity_gate_unitary(dim: int) -> np.array:
+    """Return the unitary matrix for an identity gate.
+
+    The result is the dim times dim complex identity matrix.
+
+    Parameters
+    ----------
+    dim : int
+        The dimension of the quantum system on which the gate acts.
+
+    Returns
+    ----------
+    np.array
+        The unitary matrix, which is a complex matrix.
+    """
+    u = np.eye(dim, dtype=np.complex128)
+    return u
+
+
+def generate_vec_identity_gate_hamiltonian(dim: int) -> np.array:
+    """Return the vector representation for the Hamiltonian of an identity gate with respect to the orthonormal Hermitian matrix basis with the normalized identity matrix as the 0th element.
+
+    The result is the real zero vector with size dim.
+
+    Parameters
+    ----------
+    dim : int
+        The dimension of the quantum system on which the gate acts.
+
+    Returns
+    ----------
+    np.array
+        The real vector representation of the Hamiltonian of the gate.
+    """
+    vec = np.zeros(dim, dtype=np.float64)
+    return vec
+
+
+def generate_matrix_identity_gate_lindbladian(dim: int) -> np.array:
+    """Return the Hilbert-Schmidt representation matrix for the effective Lindbladian of an Identity gate with respect to the orthonormal Hermitian matrix basis with the normalized identity matrix as the 0th element.
+
+    The result is the dim^2 times dim^2 real zero matrix.
+
+    Parameters
+    ----------
+    dim : int
+        The dimension of the quantum system on which the gate acts.
+
+    Returns
+    ----------
+    np.array
+        The real Hilbert-Schmidt representation matrix for the effective lindbladian of the gate. It is the zero matrix in this case.
+    """
+    size = dim * dim
+    mat = np.zeros((size, size), dtype=np.float64)
+    return mat
+
+
+def generate_effective_lindbladian_identity_gate(
+    c_sys: CompositeSystem,
+) -> "EffectiveLindbladian":
+    """Return the class EffectiveLindbladian for the identity gate on the composite system.
+
+    Parameters
+    ----------
+    c_sys : CompositeSystem
+        The class CompositeSystem on which the gate acts.
+
+    Returns
+    ----------
+    EffectiveLindbladian
+        The effective Lindbladian of the gate.
+    """
+    dim = c_sys.dim()
+    hs = generate_hsmatrix_identity_gate_lindbladian(dim)
+    el = EffectiveLindbladian(c_sys, hs)
+    return el
+
+
+# X90 gate on 1-qubit
