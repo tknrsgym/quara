eb014d329e4364673b291449e9be0e18f686f91d
Takanori Sugiyama <takanori@mbp20.local>
2021-03-05 13:40:21 +0900

    added state_typical.py, povm_typical.py, and gate_typical.py files

---
+++ quara/objects/qoperation_typical.py
@@ -0,0 +1,8 @@
+import numpy as np
+from quara.objects.qoperation import QOperation
+from quara.objects.state import State
+from quara.objects.povm import Povm
+from quara.objects.gate import Gate
+import quara.objects.state_typical
+import quara.objects.povm_typical
+import quara.objects.gate_typical
