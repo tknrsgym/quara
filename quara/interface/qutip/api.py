271173b986dac5dd7a7f444026d66b554bc23d8e
Kento Tokuhiro <res.circuit@gmail.com>
2021-06-22 13:47:35 +0900

    add: qutip wrapper for generating empirical dsitribution using quara

--- quara/interface/qutip/api.py
+++ quara/interface/qutip/api.py
@@ -5,6 +5,7 @@ from qutip import Qobj
 
 from quara.interface.qutip.conversion import (
     convert_gate_quara_to_qutip,
+    convert_gate_qutip_to_quara,
     convert_state_quara_to_qutip,
     convert_state_qutip_to_quara,
     convert_povm_quara_to_qutip,
@@ -45,7 +46,7 @@ def least_squares_estimator_wrapper(
         empi_dists=empi_dists,
         loss=loss,
         loss_option=loss_option,
-        alog=algo,
+        algo=algo,
         algo_option=algo_option,
         is_computation_time_required=True,
     )
@@ -86,14 +87,17 @@ def estimate_standard_povmt_for_qutip(
     num_outcomes: int,
     empi_dists: List[Tuple[int, ndarray]],
     estimator_name: str,
-    schedules: Union[List[List[int]], str],
+    schedules: Union[List[List[Tuple]], str],
 ) -> Qobj:
     c_sys = generate_composite_system(mode_system, num_system)
     tester_states_quara = []
     for qutip_state in tester_states:
         tester_states_quara.append(convert_state_qutip_to_quara(qutip_state, c_sys))
     povmt = StandardPovmt(
-        tester_states_quara, num_outcomes=num_outcomes, schedules=schedules
+        tester_states_quara,
+        num_outcomes=num_outcomes,
+        on_para_eq_constraint=True,
+        schedules=schedules,
     )
     if estimator_name == "linear":
         estimator = LinearEstimator()
@@ -115,7 +119,7 @@ def estimate_standard_qpt_for_qutip(
     tester_povms: List[Qobj],
     empi_dists: List[Tuple[int, List[int]]],
     estimator_name: str,
-    schedules: Union[List[List[int]], str],
+    schedules: Union[List[List[Tuple]], str],
 ) -> Qobj:
     c_sys = generate_composite_system(mode_system, num_system)
     tester_states_quara = []
@@ -141,3 +145,78 @@ def estimate_standard_qpt_for_qutip(
     else:
         raise ValueError("estimator_name is invalid")
     return convert_gate_quara_to_qutip(estimated_gate)
+
+
+def generate_empi_dists_from_qutip_state(
+    mode_system: str,
+    num_system: int,
+    true_state: Qobj,
+    tester_povms: List[List[Qobj]],
+    num_sum: int,
+    seed: int,
+    schedules: Union[List[List[Tuple]], str],
+) -> List[Tuple[int, ndarray]]:
+    c_sys = generate_composite_system(mode_system, num_system)
+    tester_povms_quara = []
+    for qutip_povm in tester_povms:
+        tester_povms_quara.append(convert_povm_qutip_to_quara(qutip_povm, c_sys))
+    true_state_quara = convert_state_qutip_to_quara(true_state, c_sys)
+
+    qst = StandardQst(
+        tester_povms_quara, on_para_eq_constraint=True, schedules=schedules, seed=seed
+    )
+    return qst.generate_empi_dists(state=true_state_quara, num_sum=num_sum)
+
+
+def generate_empi_dists_from_qutip_povm(
+    mode_system: str,
+    num_system: int,
+    true_povm: List[Qobj],
+    tester_states: List[Qobj],
+    num_sum: int,
+    seed: int,
+    schedules: Union[List[List[Tuple]], str],
+) -> List[Tuple[int, ndarray]]:
+    c_sys = generate_composite_system(mode_system, num_system)
+    tester_states_quara = []
+    for qutip_state in tester_states:
+        tester_states_quara.append(convert_state_qutip_to_quara(qutip_state, c_sys))
+    true_povm_quara = convert_povm_qutip_to_quara(true_povm, c_sys)
+
+    povmt = StandardPovmt(
+        tester_states_quara,
+        num_outcomes=true_povm_quara.num_outcomes,
+        on_para_eq_constraint=True,
+        schedules=schedules,
+        seed=seed,
+    )
+    return povmt.generate_empi_dists(povm=true_povm_quara, num_sum=num_sum)
+
+
+def generate_empi_dists_from_qutip_gate(
+    mode_system: str,
+    num_system: int,
+    true_gate: Qobj,
+    tester_states: List[Qobj],
+    tester_povms: List[List[Qobj]],
+    num_sum: int,
+    seed: int,
+    schedules: Union[List[List[Tuple]], str],
+) -> List[Tuple[int, ndarray]]:
+    c_sys = generate_composite_system(mode_system, num_system)
+    tester_states_quara = []
+    for qutip_state in tester_states:
+        tester_states_quara.append(convert_state_qutip_to_quara(qutip_state, c_sys))
+    tester_povms_quara = []
+    for qutip_povm in tester_povms:
+        tester_povms_quara.append(convert_povm_qutip_to_quara(qutip_povm, c_sys))
+    true_gate_quara = convert_gate_qutip_to_quara(true_gate, c_sys)
+
+    qpt = StandardQpt(
+        tester_states_quara,
+        tester_povms_quara,
+        on_para_eq_constraint=True,
+        schedules=schedules,
+        seed=seed,
+    )
+    return qpt.generate_empi_dists(gate=true_gate_quara, num_sum=num_sum)
