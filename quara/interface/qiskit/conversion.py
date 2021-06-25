ec0fc72cff7691310757f343a86730353bfc9762
kanta endo <k.endoh16@gmail.com>
2021-06-25 16:07:49 +0900

    state,povm repaired and gate to quara made

--- quara/interface/qiskit/conversion.py
+++ quara/interface/qiskit/conversion.py
@@ -5,43 +5,39 @@ from qiskit.ignis.verification.tomography.basis import TomographyBasis, default_
 from qiskit.quantum_info.operators import Operator
 from qiskit import QuantumRegister, QuantumCircuit, ClassicalRegister
 
-from quara.objects.composite_system_typical import generate_composite_system
-from quara.objects.povm_typical import generate_povm_from_name
-from quara.objects.qoperation_typical import generate_qoperation
-from quara.protocol.qtomography.standard.standard_qst import generate_empi_dists
-from quara.objects.gate_typical import generate_gate_hadamard
 from quara.objects.state import State
 from quara.objects.povm import Povm
+from quara.objects.gate import Gate
 from quara.objects.composite_system import CompositeSystem
+from quara.objects.gate_typical import (
+    calc_gate_mat_from_unitary_mat_with_hermitian_basis,
+)
 from quara.objects.matrix_basis import (
     calc_hermitian_matrix_expansion_coefficient_hermitian_basis,
 )
-from quara.utils.matrix_util import calc_mat_from_vector_adjoint
 
 
 def convert_state_qiskit_to_quara(
     qiskit_state: np.ndarray,
     c_sys: CompositeSystem,
-) -> State:  ##physicaly_correctどうしよう
+) -> State:
     qiskit_state_vec = calc_hermitian_matrix_expansion_coefficient_hermitian_basis(
         qiskit_state, c_sys.basis()
     )
-    quara_state = State(
-        c_sys=c_sys, vec=qiskit_state_vec, is_physicality_required=False
-    )
+    quara_state = State(c_sys=c_sys, vec=qiskit_state_vec)
     return quara_state
 
 
 def convert_state_quara_to_qiskit(
     quara_state: State,
 ) -> np.ndarray:
-    quara_state_vec = quara_state.vec
-    qiskit_state = calc_mat_from_vector_adjoint(quara_state_vec)
+    qiskit_state = quara_state.to_density_matrix()
     return qiskit_state
 
 
 def convert_povm_qiskit_to_quara(
-    qiskit_povm: List[np.ndarray], c_sys: CompositeSystem
+    qiskit_povm: List[np.ndarray],
+    c_sys: CompositeSystem,
 ) -> Povm:
     qiskit_povm_vec = []
     for mat in qiskit_povm:
@@ -49,21 +45,21 @@ def convert_povm_qiskit_to_quara(
             mat, c_sys.basis()
         )
         qiskit_povm_vec.append(vec)
-    quara_povm = Povm(c_sys=c_sys, vecs=qiskit_povm_vec, is_physicality_required=False)
+    quara_povm = Povm(c_sys=c_sys, vecs=qiskit_povm_vec)
     return quara_povm
 
 
-def convert_povm_quara_to_qiskit(quara_povm: Povm) -> List[np.ndarray]:
-    qiskit_povm = []
-    quara_povm_vecs = quara_povm.vecs
-    for vec in quara_povm_vecs:
-        mat = calc_mat_from_vector_adjoint(vec)  ##違う
-        qiskit_povm.append(mat)
+def convert_povm_quara_to_qiskit(
+    quara_povm: Povm,
+) -> List[np.ndarray]:
+    qiskit_povm = quara_povm.matrices()
     return qiskit_povm
 
 
 def convert_empi_dists_qiskit_to_quara(
-    qiskit_dists: np.ndarray, shots: Union[List, int], label: List[int]
+    qiskit_dists: np.ndarray,
+    shots: Union[List, int],
+    label: List[int],
 ) -> List[Tuple[int, np.ndarray]]:
     quara_dists = []
     cts = 0
@@ -81,9 +77,20 @@ def convert_empi_dists_qiskit_to_quara(
 
 
 def convert_empi_dists_quara_to_qiskit(
-    quara_dists: List[Tuple[int, np.ndarray]]
+    quara_dists: List[Tuple[int, np.ndarray]],
 ) -> np.ndarray:
     qiskit_dists = []
     for i in quara_dists:
-        qiskit_dists = qiskit_dists + i[1]  ##未確認
+        qiskit_dists = qiskit_dists + i[1]
     return qiskit_dists
+
+
+def convert_gate_qiskit_to_quara(
+    qiskit_gate: Operator,
+    c_sys: CompositeSystem,
+) -> Gate:
+    qiskit_gate_hs = calc_gate_mat_from_unitary_mat_with_hermitian_basis(
+        qiskit_gate.data, c_sys.basis()
+    )
+    quara_gate = Gate(c_sys, qiskit_gate_hs)
+    return quara_gate
