9d401991a3d3d2b1342548dfa281873c0e73a510
kanta endo <k.endoh16@gmail.com>
2021-07-02 17:04:38 +0900

    gate_typical made

---
+++ quara/interface/qiskit/qiskit_gate_typical.py
@@ -0,0 +1,37 @@
+from typing import List
+import numpy as np
+from qiskit.circuit.library.standard_gates import (
+    x,
+    y,
+    z,
+)
+
+
+def get_qiskit_povm_names_1qubit() -> List[str]:
+    return ["x", "y", "z"]
+
+
+def get_qiskit_povm_names_2qubit() -> List[str]:
+    return ["zx90"]
+
+
+def get_qiskit_povm_names_3qubit() -> List[str]:
+    return ["toffoli"]
+
+
+def generate_qiskit_gate_from_gate_name(
+    gate_name: str,
+) -> np.ndarray:
+    if gate_name == "x":
+        gate = x.XGate()
+        mat = gate.__array__
+
+    elif gate_name == "y":
+        gate = y.YGate()
+        mat = gate.__array__
+
+    elif gate_name == "z":
+        gate = z.ZGate()
+        mat = gate.__array__
+
+    return mat
