d85980cd252638344e6a633eb37549f1cff41146
komo-fr <komo.mdrms@gmail.com>
2021-07-28 14:17:27 +0900

    add eps_zero to StateEnsemble

--- quara/objects/state_ensemble.py
+++ quara/objects/state_ensemble.py
@@ -1,14 +1,25 @@
-from typing import List
-
+from typing import List, Tuple, Union
+import numpy as np
 from quara.objects.qoperation import QOperation
 from quara.objects.state import State
 from quara.objects.multinomial_distribution import MultinomialDistribution
 
 
 class StateEnsemble(QOperation):
-    def __init__(self, states: List[State], prob_dist: MultinomialDistribution):
-        self._states = states
-        self._prob_dist = prob_dist
+    def __init__(
+        self,
+        states: List[State],
+        prob_dist: MultinomialDistribution,
+        eps_zero: Union[float, np.float64] = 10 ** -8,
+    ):
+        # Validation
+        if eps_zero < 0:
+            raise ValueError("eps_zero must be a non-negative value.")
+
+        # Set
+        self._states: List[State] = states
+        self._prob_dist: MultinomialDistribution = prob_dist
+        self._eps_zero: Union[float, np.float64] = eps_zero
 
     @property  # read only
     def states(self):
@@ -17,6 +28,10 @@ class StateEnsemble(QOperation):
     @property  # read only
     def prob_dist(self):
         return self._prob_dist
+
+    @property  # read only
+    def eps_zero(self):
+        return self._eps_zero
 
     def _info(self):
         info = {}
@@ -27,3 +42,6 @@ class StateEnsemble(QOperation):
         info["MultinomialDistribution"] = self.prob_dist.__str__()
 
         return info
+
+    def state(self, outcome: Union[int, Tuple[int]]):
+        pass
