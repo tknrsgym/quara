b15638c94467233a7319faec20b5a5b3f3eaa751
kanta endo <k.endoh16@gmail.com>
2021-09-10 17:51:04 +0900

    generate qis gate modified

--- quara/interface/qiskit/qiskit_gate_typical.py
+++ quara/interface/qiskit/qiskit_gate_typical.py
@@ -1,6 +1,7 @@
 from typing import List, Union
 import numpy as np
 from qiskit.circuit.library.standard_gates import rzx, x, y, z, swap
+from qiskit.quantum_info.operators.channel import Choi
 
 
 def get_qiskit_gate_names_1qubit() -> List[str]:
@@ -20,19 +21,19 @@ def generate_qiskit_gate_from_gate_name(
 ) -> np.ndarray:
     if gate_name == "x":
         gate = x.XGate()
-        mat = gate.__array__()
+        mat = Choi(gate)
 
     elif gate_name == "y":
         gate = y.YGate()
-        mat = gate.__array__()
+        mat = Choi(gate)
 
     elif gate_name == "z":
         gate = z.ZGate()
-        mat = gate.__array__()
+        mat = Choi(gate)
 
     elif gate_name == "swap":
         gate = swap.SwapGate()
-        mat = gate.__array__()
+        mat = Choi(gate)
 
     return mat
 
