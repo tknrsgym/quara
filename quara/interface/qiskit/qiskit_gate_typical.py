eef1592d5eba51b58095d1cc566c1c37b3dec123
kanta endo <k.endoh16@gmail.com>
2021-09-10 16:53:50 +0900

    swap typical added

--- quara/interface/qiskit/qiskit_gate_typical.py
+++ quara/interface/qiskit/qiskit_gate_typical.py
@@ -35,3 +35,26 @@ def generate_qiskit_gate_from_gate_name(
         mat = gate.__array__()
 
     return mat
+
+
+def get_swap_matrix_2dim() -> np.ndarray:
+    mat = np.zeros((4, 4), dtype=np.complex64)
+    mat[0, 0] = 1
+    mat[1, 2] = 1
+    mat[2, 1] = 1
+    mat[3, 3] = 1
+    return mat
+
+
+def get_swap_matrix_3dim() -> np.ndarray:
+    mat = np.zeros((9, 9), dtype=np.complex64)
+    mat[0, 0] = 1
+    mat[1, 3] = 1
+    mat[2, 6] = 1
+    mat[3, 1] = 1
+    mat[4, 4] = 1
+    mat[5, 7] = 1
+    mat[6, 2] = 1
+    mat[7, 5] = 1
+    mat[8, 8] = 1
+    return mat
