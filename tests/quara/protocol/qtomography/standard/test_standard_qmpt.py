52241586a78443019819bbf18e8457d2f2f2bfaf
komo-fr <komo.mdrms@gmail.com>
2021-09-15 16:10:49 +0900

    add a test for calculating the probability distribution of Qmpt (1qubit)

--- tests/quara/protocol/qtomography/standard/test_standard_qmpt.py
+++ tests/quara/protocol/qtomography/standard/test_standard_qmpt.py
@@ -381,3 +381,67 @@ def test_set_coeffs():
     # Assert
     assert actual.calc_matA().shape == (48, 32)
     assert actual.calc_vecB().shape == (48,)
+
+
+def test_compare_prob_dist_1qubit():
+    # Arrange
+    c_sys = generate_composite_system(mode="qubit", num=1, ids_esys=[1])
+
+    # Tester Objects
+    state_names = ["x0", "y0", "z0", "z1"]
+    povm_names = ["x", "y", "z"]
+
+    tester_states = [
+        generate_qoperation_object(
+            mode="state", object_name="state", name=name, c_sys=c_sys
+        )
+        for name in state_names
+    ]
+    tester_povms = [
+        generate_qoperation_object(
+            mode="povm", object_name="povm", name=name, c_sys=c_sys
+        )
+        for name in povm_names
+    ]
+
+    # Qmpt
+    on_para_eq_constraint = True
+    num_outcomes = 2
+    qmpt = StandardQmpt(
+        tester_states,
+        tester_povms,
+        num_outcomes=num_outcomes,
+        on_para_eq_constraint=on_para_eq_constraint,
+        seed=7,
+    )
+
+    # TrueObject
+    true_object_name = "x-type1"
+    true_object = generate_qoperation_object(
+        mode="mprocess", object_name="mprocess", name=true_object_name, c_sys=c_sys
+    )
+
+    schedule_n = len(qmpt._experiment.schedules)  # 12
+    actual_list = []
+    start = 0
+
+    # Act
+    for schedule_index in range(schedule_n):
+        povm_index = qmpt._experiment.schedules[schedule_index][2][1]
+        povm = qmpt._experiment.povms[povm_index]
+        num = qmpt.num_outcomes * povm._num_outcomes
+        end = start + num
+
+        A = qmpt.calc_matA()[start:end]
+        b = qmpt.calc_vecB()[start:end]
+
+        p = A @ true_object.to_var() + b
+
+        actual_list.append(np.array(p))
+        start = end
+
+    # Assert
+    expected_list = qmpt.calc_prob_dists(true_object)
+
+    for actual, expected in zip(actual_list, expected_list):
+        npt.assert_almost_equal(actual, expected, decimal=15)
