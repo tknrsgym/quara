4b741040340825f62acf3db49e730b3960519c19
komo-fr <komo.mdrms@gmail.com>
2021-09-29 14:26:50 +0900

    fix test code for qmpt

--- tests/quara/protocol/qtomography/standard/test_standard_qmpt.py
+++ tests/quara/protocol/qtomography/standard/test_standard_qmpt.py
@@ -497,24 +497,8 @@ def test_compare_prob_dist_1qubit(on_para_eq_constraint: bool):
             hss=true_object.hss, on_para_eq_constraint=False, c_sys=c_sys
         )
 
-    schedule_n = len(qmpt._experiment.schedules)  # 12
-    actual_list = []
-    start = 0
-
     # Act
-    for schedule_index in range(schedule_n):
-        povm_index = qmpt._experiment.schedules[schedule_index][2][1]
-        povm = qmpt._experiment.povms[povm_index]
-        num = qmpt.num_outcomes * povm._num_outcomes
-        end = start + num
-
-        A = qmpt.calc_matA()[start:end]
-        b = qmpt.calc_vecB()[start:end]
-
-        p = A @ true_object.to_var() + b
-
-        actual_list.append(np.array(p))
-        start = end
+    actual_list = qmpt.calc_prob_dists(true_object)
 
     # Assert
     expected_list = calc_prob_dist_with_experiment(qmpt, true_object)
@@ -564,30 +548,15 @@ def test_compare_prob_dist_2qubit(on_para_eq_constraint: bool):
         seed=7,
     )
 
-    schedule_n = len(qmpt._experiment.schedules)  # 144
-    actual_list = []
-    start = 0
-
     # Act
-    for schedule_index in range(schedule_n):
-        povm_index = qmpt._experiment.schedules[schedule_index][2][1]
-        povm = qmpt._experiment.povms[povm_index]
-        num = qmpt.num_outcomes * povm._num_outcomes
-        end = start + num
-
-        A = qmpt.calc_matA()[start:end]
-        b = qmpt.calc_vecB()[start:end]
-
-        p = A @ true_object.to_var() + b
-
-        actual_list.append(np.array(p))
-        start = end
+    actual_list = qmpt.calc_prob_dists(true_object)
 
     # Assert
     expected_list = calc_prob_dist_with_experiment(qmpt, true_object)
 
     for actual, expected in zip(actual_list, expected_list):
-        npt.assert_almost_equal(actual, expected, decimal=15)
+        # If decimal is set to 15, the test will fail.
+        npt.assert_almost_equal(actual, expected, decimal=14)
 
 
 @pytest.mark.parametrize(("on_para_eq_constraint"), [(True), (False)])
@@ -639,27 +608,11 @@ def test_compare_prob_dist_1qutrit(on_para_eq_constraint: bool):
         true_object = MProcess(
             hss=true_object.hss, on_para_eq_constraint=False, c_sys=c_sys
         )
+    # Act
+    actual_list = qmpt.calc_prob_dists(true_object)
 
-    schedule_n = len(qmpt._experiment.schedules)
-    p_list = []
-    start = 0
-
-    for schedule_index in range(schedule_n):
-        povm_index = qmpt._experiment.schedules[schedule_index][2][1]
-        povm = qmpt._experiment.povms[povm_index]
-        num = qmpt.num_outcomes * povm._num_outcomes
-        end = start + num
-
-        A = qmpt.calc_matA()[start:end]
-        b = qmpt.calc_vecB()[start:end]
-
-        p = A @ true_object.to_var() + b
-
-        p_list.append(np.array(p))
-        start = end
-
+    # Assert
     expected_list = calc_prob_dist_with_experiment(qmpt, true_object)
-    actual_list = p_list
     for actual, expected in zip(actual_list, expected_list):
         npt.assert_almost_equal(actual, expected, decimal=15)
 
