82dcbaef7e9a83dbd3c970aaaf908fee69eb494b
Kento Tokuhiro <res.circuit@gmail.com>
2021-10-11 15:55:49 +0900

    add; forest api for state preparation

--- quara/interface/forest/api.py
+++ quara/interface/forest/api.py
@@ -2,11 +2,68 @@ from itertools import product
 from typing import List, Tuple
 
 import numpy as np
+from numpy import pi
 from qutip import Qobj, identity, sigmax, sigmay, sigmaz, tensor
 from scipy.linalg import pinv
+from pyquil import Program
+from pyquil.gates import H, X, RX
 
 from quara.interface.qutip.conversion import convert_povm_quara_to_qutip
 from quara.objects.povm import Povm
+
+
+def generate_program_for_1qubit(qubit: int, state_name: str) -> Program:
+    """Generates a pyquil program that prepares the desired for 1 qubit.
+
+    Parameters
+    ----------
+    qubit: int
+        Index of the target qubit.
+    state_name: str
+        Name of the desired state which is z0, x0 or y0.
+
+    Returns
+    -------
+    Program
+        Program for state preparation for the specific qubit
+    """
+    if state_name == "z0":
+        return Program()
+    elif state_name == "z1":
+        return Program(X(qubit))
+    elif state_name == "x0":
+        return Program(H(qubit))
+    elif state_name == "x1":
+        return Program(X(qubit), H(qubit))
+    elif state_name == "y0":
+        return Program(RX(-pi / 2, qubit))
+    elif state_name == "y1":
+        return Program(RX(pi / 2, qubit))
+    else:
+        raise ValueError("invalid state_name")
+
+
+def generate_preprocess_program(qubits: List[int], state_name: str) -> Program:
+    """Generates a pyquil program for the state preparation of the qubit system.
+
+    Parameters
+    ----------
+    qubits: List[int]
+        Index configuration of the qubit system.
+    state_name: str
+        Name of the desired state.
+
+    Returs
+    ------
+    Program
+        Program for the state preparation of the system
+    """
+    state_names_1qubit = state_name.split("_")
+    assert len(qubits) == len(state_names_1qubit)
+    pre_process_program = Program()
+    for qubit, state_name_1qubit in zip(qubits, state_names_1qubit):
+        pre_process_program += generate_program_for_1qubit(qubit, state_name_1qubit)
+    return pre_process_program
 
 
 def calc_empi_dist_from_observables(
