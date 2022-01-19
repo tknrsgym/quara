075b54170d06881279597adf79e4e35c4a5ea25c
komo-fr <komo.mdrms@gmail.com>
2022-01-19 14:52:51 +0900

    improve _calc_basis_basisconjugate_sparse

--- quara/objects/composite_system.py
+++ quara/objects/composite_system.py
@@ -377,51 +377,93 @@ class CompositeSystem:
     #         basishermitian_basis_tmp_from_1.T
     #     )
 
+    # @profile
+    # def _calc_basis_basisconjugate_sparse(self) -> None:
+    #     basis_no = len(self._total_basis.basis)
+    #     basis = copy.deepcopy(self._total_basis.basis)
+
+    #     basis_basisconjugate_tmp = []
+    #     basis_basisconjugate_tmp_from_1 = []
+    #     basishermitian_basis_tmp_from_1 = []
+    #     element_size = basis[0].size ** 2 ** 2
+    #     element_size_2 = basis[0].size ** 2
+
+    #     print(f"{basis_no=}")
+    #     count = 0
+    #     for alpha, beta in tqdm(itertools.product(range(basis_no), range(basis_no))):
+    #         b_alpha = basis[alpha]
+    #         b_beta_conj = np.conjugate(basis[beta])
+    #         matrix = sparse.kron(b_alpha, b_beta_conj, format="csr")
+    #         reshaped_matrix = matrix.reshape(1, element_size)
+    #         basis_basisconjugate_tmp.append(reshaped_matrix)
+
+    #         if alpha != 0 and beta != 0:
+    #             count += 1
+    #             basis_basisconjugate_tmp_from_1.append(reshaped_matrix)
+
+    #             matrix_2 = basis[beta].conj().T @ b_alpha
+    #             basishermitian_basis_tmp_from_1.append(
+    #                 matrix_2.reshape(1, element_size_2)
+    #             )
+
+    #     # set _basisconjugate_basis_sparse and _basis_basisconjugate_T_sparse
+    #     basis_basisconjugate_tmp = sparse.vstack(basis_basisconjugate_tmp).reshape(
+    #         len(basis_basisconjugate_tmp), element_size
+    #     )
+    #     self._basisconjugate_basis_sparse = basis_basisconjugate_tmp.conjugate()
+    #     self._basis_basisconjugate_T_sparse = basis_basisconjugate_tmp.T
+
+    #     # set _basis_basisconjugate_T_sparse_from_1
+    #     basis_basisconjugate_tmp_from_1 = sparse.vstack(
+    #         basis_basisconjugate_tmp_from_1
+    #     ).reshape(len(basis_basisconjugate_tmp_from_1), element_size)
+    #     self._basis_basisconjugate_T_sparse_from_1 = basis_basisconjugate_tmp_from_1.T
+
+    #     # set _basishermitian_basis_T_from
+    #     basishermitian_basis_tmp_from_1 = sparse.vstack(
+    #         basishermitian_basis_tmp_from_1
+    #     ).reshape(len(basishermitian_basis_tmp_from_1), element_size_2)
+    #     self._basishermitian_basis_T_from_1 = basishermitian_basis_tmp_from_1.T
+
     @profile
     def _calc_basis_basisconjugate_sparse(self) -> None:
         basis_no = len(self._total_basis.basis)
         basis = copy.deepcopy(self._total_basis.basis)
 
-        basis_basisconjugate_tmp = []
-        basis_basisconjugate_tmp_from_1 = []
-        basishermitian_basis_tmp_from_1 = []
+        basis_basisconjugate_tmp = None
+        basis_basisconjugate_tmp_from_1 = None
+        basishermitian_basis_tmp_from_1 = None
         element_size = basis[0].size ** 2 ** 2
         element_size_2 = basis[0].size ** 2
 
         print(f"{basis_no=}")
+        count = 0
 
         for alpha, beta in tqdm(itertools.product(range(basis_no), range(basis_no))):
             b_alpha = basis[alpha]
             b_beta_conj = np.conjugate(basis[beta])
             matrix = sparse.kron(b_alpha, b_beta_conj, format="csr")
             reshaped_matrix = matrix.reshape(1, element_size)
-            basis_basisconjugate_tmp.append(reshaped_matrix)
+            basis_basisconjugate_tmp = reshaped_matrix if basis_basisconjugate_tmp is None else sparse.vstack([basis_basisconjugate_tmp, reshaped_matrix])
 
             if alpha != 0 and beta != 0:
-                basis_basisconjugate_tmp_from_1.append(reshaped_matrix)
+                count += 1
+                basis_basisconjugate_tmp_from_1 = reshaped_matrix if basis_basisconjugate_tmp_from_1 is None else sparse.vstack([basis_basisconjugate_tmp_from_1, reshaped_matrix])
 
-                matrix_2 = basis[beta].conj().T @ b_alpha
-                basishermitian_basis_tmp_from_1.append(
-                    matrix_2.reshape(1, element_size_2)
-                )
+                reshaped_matrix_2 = (basis[beta].conj().T @ b_alpha).reshape(1, element_size_2)
+                basishermitian_basis_tmp_from_1 = reshaped_matrix_2 if basishermitian_basis_tmp_from_1 is None else sparse.vstack([basishermitian_basis_tmp_from_1, reshaped_matrix_2])
 
         # set _basisconjugate_basis_sparse and _basis_basisconjugate_T_sparse
-        basis_basisconjugate_tmp = sparse.vstack(basis_basisconjugate_tmp).reshape(
-            len(basis_basisconjugate_tmp), element_size
-        )
+        basis_basisconjugate_tmp = basis_basisconjugate_tmp.reshape(basis_no ** 2, element_size)
         self._basisconjugate_basis_sparse = basis_basisconjugate_tmp.conjugate()
         self._basis_basisconjugate_T_sparse = basis_basisconjugate_tmp.T
 
         # set _basis_basisconjugate_T_sparse_from_1
-        basis_basisconjugate_tmp_from_1 = sparse.vstack(
-            basis_basisconjugate_tmp_from_1
-        ).reshape(len(basis_basisconjugate_tmp_from_1), element_size)
+        basis_basisconjugate_tmp_from_1 = basis_basisconjugate_tmp_from_1.reshape(count, element_size)
         self._basis_basisconjugate_T_sparse_from_1 = basis_basisconjugate_tmp_from_1.T
 
         # set _basishermitian_basis_T_from
-        basishermitian_basis_tmp_from_1 = sparse.vstack(
-            basishermitian_basis_tmp_from_1
-        ).reshape(len(basishermitian_basis_tmp_from_1), element_size_2)
+        basishermitian_basis_tmp_from_1 = basishermitian_basis_tmp_from_1.reshape(count, element_size_2)
         self._basishermitian_basis_T_from_1 = basishermitian_basis_tmp_from_1.T
 
     @property
